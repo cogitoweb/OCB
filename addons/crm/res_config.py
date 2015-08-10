cogitoweb/OCB

commit d3b04b65fcff7555d07226e386028dda25b3bebb

[IMP] All res_config files : Convert Booleans to Selection fields + radio buttons in views

addons/crm/res_config.py
@@ -10,17 +10,16 @@ class crm_configuration(osv.TransientModel):
     _inherit = ['sale.config.settings', 'fetchmail.config.settings']
 
     _columns = {
-        'group_fund_raising': fields.boolean("Manage Fund Raising",
-            implied_group='crm.group_fund_raising',
-            help="""Allows you to trace and manage your activities for fund raising."""),
-        'module_crm_claim': fields.boolean("Manage Customer Claims",
-            help='Allows you to track your customers/vendors claims and grievances.\n'
-                 '-This installs the module crm_claim.'),
         'generate_sales_team_alias': fields.boolean(
             "Automatically generate an email alias at the sales team creation",
             help="Odoo will generate an email alias based on the sales team name"),
         'alias_prefix': fields.char('Default Alias Name for Leads'),
-        'alias_domain' : fields.char('Alias Domain')
+        'alias_domain' : fields.char('Alias Domain'),
+        'group_multi_salesteams': fields.selection([
+            (0, 'One sales team only'),
+            (1, 'Activate multiple sales teams')
+            ], "Multiple Sales Teams",
+            implied_group='base.group_multi_salesteams'),
     }
 
     _defaults = {

addons/crm/res_config_view.xml
@@ -7,51 +7,37 @@
             <field name="inherit_id" ref="base_setup.view_sale_config_settings"/>
             <field name="arch" type="xml">
                 <data>
-	            <div name="config_sale" position="inside">
-	                <separator string="Sale Services"/>
-                    <group>
-                        <label for="id" string="After-Sale Services"/>
+                <div id="main" position="inside">
+                    <group string="CRM" name="config_alias">
+                        <label for="id" string="Leads Email Alias"/>
                         <div>
-                            <div>
-                                <field name="module_crm_claim" class="oe_inline"/>
-                                <label for="module_crm_claim"/>
+                            <div attrs="{'invisible': [('alias_domain', 'in', ['localhost', '', False])]}">
+                                <p class="text-muted">
+                                    Each sales team has an email alias that
+                                    could be setup on the sales team form. You
+                                    can setup a generic email alias to create
+                                    incoming leads.
+                                </p>
+                                <field name="alias_prefix" class="oe_inline"/>
+                                <span>@</span>
+                                <field name="alias_domain"  class="oe_inline" readonly="1"/>
+                            </div>
+                            <div attrs="{'invisible': [('alias_domain', 'not in', ['localhost', '', False])]}">
+                                <p>
+                                    <button type="action"
+                                        name="base_setup.action_general_configuration"
+                                        string="Setup your domain alias" class="oe_link"/>
+                                </p>
                             </div>
                         </div>
+
+                        <label for="group_multi_salesteams"/>
+                        <div>
+                            <field name="group_multi_salesteams" widget="radio"/>
+                        </div>
+
                     </group>
                 </div>
-                <div name="config_sale" position="after">
-                    <div name="config_alias">
-                        <separator string="Emails Integration"/>
-                        <p name="config_email_integration">
-                            Odoo allows to automatically create leads
-                            from incoming emails.
-                        </p> 
-                        <group>
-                            <label for="id" string="Leads Email Alias"/>
-                            <div>
-                                 <div attrs="{'invisible': [('alias_domain', 'not in', ['localhost', '', False])]}">
-                                    <p>
-                                        <label string="To be able to set your Leads Email Alias, you need to fill your alias domain first."/><br />
-                                        <button type="action" name="base_setup.action_general_configuration"
-                                                string="Configure your email alias domain" class="oe_inline oe_link"/>
-                                    </p>
-                                </div>
-                                <div>
-                                    <span class="oe_inline">
-                                        <label string="Create new leads when an email is sent to the following alias:" class="oe_inline oe_form_label_help oe_align_right"/>
-                                        <field name="alias_prefix" class="oe_inline"/>
-                                        <span>@</span>
-                                        <field name="alias_domain" class="oe_inline" readonly="1"/>
-                                    </span>
-                                </div>
-                                <div>
-                                    <field name="generate_sales_team_alias"/>
-                                    <label for="generate_sales_team_alias"/>
-                                </div>
-                            </div>
-                        </group>
-                    </div>
-	            </div>
                 </data>
             </field>
         </record>

openerp/addons/base/res/res_config.py
@@ -402,6 +402,10 @@ class res_config_settings(osv.osv_memory, res_config_module_installation_mixin):
         *   For a boolean field like 'module_XXX', ``execute`` triggers the immediate
             installation of the module named 'XXX' if the field has value ``True``.
 
+        *   For a selection field like 'module_XXX' composed of 2 integers values ('0' and '1'), 
+            ``execute`` triggers the immediate installation of the module named 'XXX' 
+            if the field has the integer value ``1``.
+
         *   For the other fields, the method ``execute`` invokes all methods with a name
             that starts with 'set_'; such methods can be defined to implement the effect
             of those fields.
@@ -493,7 +497,7 @@ class res_config_settings(osv.osv_memory, res_config_module_installation_mixin):
             elif name.startswith('group_') and isinstance(field, fields.boolean) and hasattr(field, 'implied_group'):
                 field_groups = getattr(field, 'group', 'base.group_user').split(',')
                 groups.append((name, map(ref, field_groups), ref(field.implied_group)))
-            elif name.startswith('module_') and isinstance(field, fields.boolean):
+            elif name.startswith('module_') and (isinstance(field, fields.boolean) or isinstance(field, fields.selection)):
                 mod_ids = ir_module.search(cr, uid, [('name', '=', name[7:])])
                 record = ir_module.browse(cr, uid, mod_ids[0], context) if mod_ids else None
                 modules.append((name, record))
@@ -521,6 +525,8 @@ class res_config_settings(osv.osv_memory, res_config_module_installation_mixin):
         # modules: which modules are installed/to install
         for name, module in classified['module']:
             res[name] = module and module.state in ('installed', 'to install', 'to upgrade')
+            if self._fields[name].type == 'selection':
+                res[name] = int(res[name])
 
         # other fields: call all methods that start with 'get_default_'
         for method in dir(self):

addons/base_setup/res_config.py
@@ -80,12 +80,10 @@ class base_config_settings(osv.osv_memory):
             self.pool['ir.rule'].write(cr, uid, [partner_rule.id], {'active': not bool(wizard.company_share_partner)}, context=context)
 
 
-# Preferences wizard for Sales & CRM.
-# It is defined here because it is inherited independently in modules sale, crm.
+# Empty class but required since it's overrided by sale & crm
 class sale_config_settings(osv.osv_memory):
     _name = 'sale.config.settings'
     _inherit = 'res.config.settings'
     _columns = {
-        'module_crm': fields.boolean('CRM'),
-        'module_sale' : fields.boolean('SALE'),
     }
+

addons/base_setup/res_config_view.xml
@@ -20,15 +20,6 @@
                         </p>
                     </div>
                     <group>
-                        <label for="id" string="Options"/>
-                        <div>
-                            <div name="multi_company">
-                                <field name="module_multi_company" class="oe_inline"/>
-                                <label for="module_multi_company"/>
-                            </div>
-                        </div>
-                    </group>
-                    <group>
                         <label for="id" string="Email" name="email_label"/>
                         <div name="email">
                             <div name="email.outgoing_server">
@@ -38,7 +29,7 @@
                             </div>
                         </div>
                     </group>
-                    <group>
+                    <group groups="base.group_no_one">
                         <label for="id" string="Portal access"/>
                         <div>
                             <div>
@@ -72,6 +63,42 @@
                             </div>
                         </div>
                     </group>
+                    <group groups="base.group_no_one">
+                        <label for="font" />
+                        <div>
+                            <div>
+                                <field name="font" class="oe_inline" />
+                                <button string="(reload fonts)" name="act_discover_fonts" type="object" class="oe_link"/>
+                            </div>
+                        </div>
+                    </group>
+                    <group>
+                        <label for="id" string="Multi Company"/>
+                        <div>
+                            <div name="multi_company">
+                                <field name="module_multi_company" class="oe_inline"/>
+                                <label for="module_multi_company"/>
+                            </div>
+                        </div>
+                    </group>
+                    <group attrs="{'invisible': [('module_multi_company','=',False)]}">
+                        <label for="module_inter_company_rules" string="Inter company"/>
+                        <div name="inter_company">
+                            <div name="module_inter_company_rules">
+                                <field name="module_inter_company_rules" class="oe_inline"/>
+                                <label for="module_inter_company_rules"/>
+                            </div>
+                        </div>
+                    </group>
+                    <group string="Shared ressources" name="company_share_ressources" attrs="{'invisible': [('module_multi_company','=',False)]}">
+                        <label for="company_share_partner" />
+                        <div>
+                            <div>
+                                <field name="company_share_partner" class="oe_inline" />
+                            </div>
+                        </div>
+                    </group>
+                    <separator string="Google Integration" name="google"/>
                     <group>
                         <label for="id" string="Google Drive"/>
                         <div name="google_drive">
@@ -93,32 +120,6 @@
                                         </p>
                                     </div>
                                 </div>
-                        </div>
-                    </group>
-                    <group>
-                        <label for="font" />
-                        <div>
-                            <div>
-                                <field name="font" class="oe_inline" />
-                                <button string="(reload fonts)" name="act_discover_fonts" type="object" class="oe_link"/>
-                            </div>
-                        </div>
-                    </group>
-                    <group>
-                        <label for="module_inter_company_rules" string="Inter company"/>
-                        <div name="inter_company">
-                            <div name="module_inter_company_rules">
-                                <field name="module_inter_company_rules" class="oe_inline"/>
-                                <label for="module_inter_company_rules"/>
-                            </div>
-                        </div>
-                    </group>
-                    <group string="Shared ressources" name="company_share_ressources">
-                        <label for="company_share_partner" />
-                        <div>
-                            <div>
-                                <field name="company_share_partner" class="oe_inline" />
-                            </div>
                         </div>
                     </group>
                 </form>
@@ -145,20 +146,9 @@
                         or
                         <button string="Cancel" type="object" name="cancel" class="oe_link"/>
                     </header>
-                    <div name="customer feature" attrs="{'invisible': [('module_sale','=',False)]}">
-                        <field name="module_sale" invisible="1"/>
-                        <separator string="Quotations and Sales Orders"/>
-                        <group name="Customer">
-                            <label for="id" string="Customer Features"/>
-                            <div name="Customer Features"/>
-                        </group>
-                        <group name="Sale">
-                            <label for="id" string="Sale Features"/>
-                            <div name="Sale Features"/>
-                        </group>
+                    <div id="main">
+
                     </div>
-                    <div name="config_sale"/>
-                    <div name="config_pos"/>
                 </form>
             </field>
         </record>

addons/event/res_config.py
@@ -6,19 +6,30 @@ class event_config_settings(osv.TransientModel):
     _name='event.config.settings'
     _inherit='res.config.settings'
     _columns = {
-        'module_event_sale': fields.boolean(
-            'Sell events tickets',
+        'module_event_sale': fields.selection([
+            (0, "All events are free"),
+            (1, 'Allow selling tickets')
+            ], "Tickets",
             help='Install the event_sale module'),
-        'module_website_event_track': fields.boolean(
-            'Organize few days event with track, full agenda, own menu in website',
+        'module_website_event_track': fields.selection([
+            (0, "No mini website per event"),
+            (1, 'Allow tracks, agenda and dedicated menus/website per event')
+            ], "Tracks and Agenda",
             help='Install the module website_event_track'),
-        'module_website_event_questions': fields.boolean(
-            'Ask questions to online subscribers',
+        'module_website_event_questions': fields.selection([
+            (0, "No extra questions on subscriptions"),
+            (1, 'Allow adding extra questions on subscriptions')
+            ], "Subscription Survey",
             help='Install the website_event_questions module'),
-        'auto_confirmation': fields.boolean(
-            'Automate events and subscription confirmation', help='Unselect this option to manually manage draft event and draft subscription'),
-        'group_email_scheduling': fields.boolean(
-            'Schedule emails to attendees and subscribers', 
+        'auto_confirmation': fields.selection([
+            (1, 'No validation step on subscription'),
+            (0, "Manually confirm every subscription")
+            ], "Auto Confirmation",
+            help='Unselect this option to manually manage draft event and draft subscription'),
+        'group_email_scheduling': fields.selection([
+            (0, "No automated emails"),
+            (1, 'Schedule emails to attendees and subscribers')
+            ], "Email Scheduling",
             help='You will be able to configure emails, and to schedule them to be automatically sent to the attendees on subscription and/or attendance',
             implied_group='event.group_email_scheduling'),            
     }

addons/event/res_config_view.xml
@@ -12,30 +12,31 @@
                         or
                         <button string="Cancel" type="object" name="cancel" class="oe_link"/>
                     </header>
-                    <separator string="Mass Mailing"/>
+                    <separator string="Events"/>
                     <group>
-                        <label for="id" string="Settings"/>
-                        <div>                        
-                            <div name="module_event_sale">    
-                                <field name="module_event_sale" class="oe_inline"/>
-                                <label for="module_event_sale"/>
-                            </div>
-                            <div name="module_website_event_track">
-                                <field name="module_website_event_track"/>
-                                <label for="module_website_event_track"/>
-                            </div>
-                            <div name="div_website_event_questions">
-                                <field name="module_website_event_questions"/>
-                                <label for="module_website_event_questions"/>
-                            </div>
-                            <div name="div_auto_confirmation">
-                                <field name="auto_confirmation"/>
-                                <label for="auto_confirmation"/>
-                            </div>
-                            <div name="div_email_scheduling">
-                                <field name="group_email_scheduling"/>
-                                <label for="group_email_scheduling"/>
-                            </div>
+                        <label for="module_event_sale"/>
+                        <div>
+                            <field name="module_event_sale" widget="radio"/>
+                        </div>
+
+                        <label for="module_website_event_track"/>
+                        <div>
+                            <field name="module_website_event_track" widget="radio"/>
+                        </div>
+
+                        <label for="module_website_event_questions"/>
+                        <div>
+                            <field name="module_website_event_questions" widget="radio"/>
+                        </div>
+
+                        <label for="auto_confirmation"/>
+                        <div>
+                            <field name="auto_confirmation" widget="radio"/>
+                        </div>
+
+                        <label for="group_email_scheduling"/>
+                        <div>
+                            <field name="group_email_scheduling" widget="radio"/>
                         </div>
                     </group>
                 </form>

addons/google_calendar/res_config.py
@@ -4,7 +4,7 @@ class calendar_config_settings(osv.TransientModel):
     _inherit = 'base.config.settings'
     
     _columns = {
-        'google_cal_sync': fields.boolean("Show tutorial to know how to get my 'Client ID' and my 'Client Secret'"),
+        'google_cal_sync': fields.boolean("Show Tutorial"),
         'cal_client_id': fields.char("Client_id"),
         'cal_client_secret': fields.char("Client_key"),
         'server_uri': fields.char('URI for tuto')

commit 6b31cbfa726b44d8dafa4b0758dd154fc603da45

[FIX] base : selection fields starting with 'group_' were not taken into account Bug introduced by https://github.com/odoo/odoo/commit/d3b04b65fcff7555d07226e386028dda25b3bebb

addons/crm/res_config.py
@@ -15,11 +15,6 @@ class crm_configuration(osv.TransientModel):
             help="Odoo will generate an email alias based on the sales team name"),
         'alias_prefix': fields.char('Default Alias Name for Leads'),
         'alias_domain' : fields.char('Alias Domain'),
-        'group_multi_salesteams': fields.selection([
-            (0, 'One sales team only'),
-            (1, 'Activate multiple sales teams')
-            ], "Multiple Sales Teams",
-            implied_group='base.group_multi_salesteams'),
     }
 
     _defaults = {

addons/crm/res_config_view.xml
@@ -31,11 +31,6 @@
                             </div>
                         </div>
 
-                        <label for="group_multi_salesteams"/>
-                        <div>
-                            <field name="group_multi_salesteams" widget="radio"/>
-                        </div>
-
                     </group>
                 </div>
                 </data>

openerp/addons/base/res/res_config.py
@@ -399,6 +399,12 @@ class res_config_settings(osv.osv_memory, res_config_module_installation_mixin):
             By default 'group' is the group Employee.  Groups are given by their xml id.
             The attribute 'group' may contain several xml ids, separated by commas.
 
+        *   For a selection field like 'group_XXX' composed of 2 integers values ('0' and '1'),
+            ``execute`` adds/removes 'implied_group' to/from the implied groups of 'group', 
+            depending on the field's value.
+            By default 'group' is the group Employee.  Groups are given by their xml id.
+            The attribute 'group' may contain several xml ids, separated by commas.
+
         *   For a boolean field like 'module_XXX', ``execute`` triggers the immediate
             installation of the module named 'XXX' if the field has value ``True``.
 
@@ -494,7 +500,8 @@ class res_config_settings(osv.osv_memory, res_config_module_installation_mixin):
         for name, field in self._columns.items():
             if name.startswith('default_') and hasattr(field, 'default_model'):
                 defaults.append((name, field.default_model, name[8:]))
-            elif name.startswith('group_') and isinstance(field, fields.boolean) and hasattr(field, 'implied_group'):
+            elif name.startswith('group_') and (isinstance(field, fields.boolean) or isinstance(field, fields.selection)) \
+                 and hasattr(field, 'implied_group'):
                 field_groups = getattr(field, 'group', 'base.group_user').split(',')
                 groups.append((name, map(ref, field_groups), ref(field.implied_group)))
             elif name.startswith('module_') and (isinstance(field, fields.boolean) or isinstance(field, fields.selection)):