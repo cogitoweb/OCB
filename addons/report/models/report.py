cogitoweb/OCB

commit 98c19729e4b094373dc5516fd6a5d14c26b54dd2

[FIX] report: use hardcoded viewport in wkhtmltopdf to use col-md-* Bootstrap
Without this, wkhtmltopdf, don't use media query from bootstrap and so respect
only col-xs-* class (xs by default because no media query).

Viewport from wkhtmltopdf is not really respected, so 1024x768 or 1920x768 will
not change the real view port, but only use the right media query.

addons/report/models/report.py
@@ -375,6 +375,8 @@ class Report(osv.Model):
         :returns: Content of the pdf as a string
         """
         command_args = []
+        if wkhtmltopdf_state == "ok":
+            command_args.extend(['--viewport-size', '1024x768'])
 
         # Passing the cookie to wkhtmltopdf in order to resolve internal links.
         try:

commit a32e322b20844eaa62e4db1b025cd3d426974685

[FIX] report: manage landscape view port for wkhtml
Bug introduced by commit 98c1972

Fix #7960

addons/report/models/report.py
@@ -376,7 +376,7 @@ class Report(osv.Model):
         """
         command_args = []
         if wkhtmltopdf_state == "ok":
-            command_args.extend(['--viewport-size', '1024x768'])
+            command_args.extend(['--viewport-size', landscape and '1024x1280' or '1280x1024'])
 
         # Passing the cookie to wkhtmltopdf in order to resolve internal links.
         try:
@@ -399,7 +399,7 @@ class Report(osv.Model):
                     del command_args[index]
                     del command_args[index]
                     command_args.extend(['--orientation', 'landscape'])
-        elif landscape and not '--orientation' in command_args:
+        elif landscape and '--orientation' not in command_args:
             command_args.extend(['--orientation', 'landscape'])
 
         # Execute WKhtmltopdf
@@ -449,7 +449,7 @@ class Report(osv.Model):
 
                 if process.returncode not in [0, 1]:
                     raise UserError(_('Wkhtmltopdf failed (error code: %s). '
-                                        'Message: %s') % (str(process.returncode), err))
+                                      'Message: %s') % (str(process.returncode), err))
 
                 # Save the pdf in attachment if marked
                 if reporthtml[0] is not False and save_in_attachment.get(reporthtml[0]):
@@ -464,8 +464,7 @@ class Report(osv.Model):
                         try:
                             self.pool['ir.attachment'].create(cr, uid, attachment)
                         except AccessError:
-                            _logger.info("Cannot save PDF report %r as attachment",
-                                            attachment['name'])
+                            _logger.info("Cannot save PDF report %r as attachment", attachment['name'])
                         else:
                             _logger.info('The PDF document %s is now saved in the database',
                                          attachment['name'])