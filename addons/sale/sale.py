cogitoweb/OCB

commit 9d25613f5397d4c1ca2d035e429c11c511b5974c

[IMP] better error message (draft quotation does not exist), allow deleting canceled quotations

addons/sale/sale.py
@@ -148,8 +148,8 @@ class SaleOrder(models.Model):
     @api.multi
     def unlink(self):
         for order in self:
-            if order.state != 'draft':
-                raise UserError(_('You can only delete draft quotations!'))
+            if order.state not in ('draft', 'cancel'):
+                raise UserError(_('You can not delete a quotation sent or a sale order! Try to cancel it before.'))
         return super(SaleOrder, self).unlink()
 
     @api.multi

commit 45a7fb23c53576dea3ad05343ed8a6f9fdc93448

[FIX] sale: typos
Commits 91d2c2488e109bdd8638ef844f8d46b8468189e3 and 9d25613f5397d4c1ca2d035e429c11c511b5974c
introduced typos:

 - sale order instead of sales order
 - sent quotation instead of quotation sent

addons/sale/sale.py
@@ -149,7 +149,7 @@ class SaleOrder(models.Model):
     def unlink(self):
         for order in self:
             if order.state not in ('draft', 'cancel'):
-                raise UserError(_('You can not delete a quotation sent or a sale order! Try to cancel it before.'))
+                raise UserError(_('You can not delete a sent quotation or a sales order! Try to cancel it before.'))
         return super(SaleOrder, self).unlink()
 
     @api.multi

addons/sale/sale_view.xml
@@ -388,7 +388,7 @@
                 <p class="oe_view_nocontent_create">
                     Create a Quotation, the first step of a new sale.
                 </p><p>
-                    Once the quotation is confirmed, it becomes a sale order.
+                    Once the quotation is confirmed, it becomes a sales order.
                     You'll be able to invoice it and collect payments.
                     From the <i>Sales Orders</i> menu, you can track delivery
                     orders or services.