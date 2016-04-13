odoo/odoo

[MOVE] website_mail_channel: file renaming according to new api

addons/website_mail_channel/__openerp__.py
@@ -10,7 +10,7 @@ Odoo Mail Group : Mailing List Archives
         """,
     'depends': ['website_mail'],
     'data': [
-        'views/website_mail_channel.xml',
+        'views/mail_channel_templates.xml',
         'views/snippets.xml',
     ],
     'qweb': [],

cogitoweb/OCB

addons/website_mail_channel/__openerp__.py
@@ -10,7 +10,7 @@ Odoo Mail Group : Mailing List Archives
         """,
     'depends': ['website_mail'],
     'data': [
-        'views/website_mail_channel.xml',
+        'views/website_mail_channel_templates.xml',
         'views/snippets.xml',
     ],
     'qweb': [],