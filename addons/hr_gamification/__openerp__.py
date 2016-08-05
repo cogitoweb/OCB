odoo/odoo

[MIG] hr_gamification: Restructured the module files as per new guidelines.

addons/hr_gamification/__openerp__.py
@@ -15,9 +15,10 @@ Badge received are displayed on the user profile.
     'data': [
         'security/ir.model.access.csv',
         'security/gamification_security.xml',
-        'wizard/grant_badge.xml',
-        'views/gamification.xml',
-        'views/hr_gamification.xml',
+        'wizard/gamification_badge_user_wizard_views.xml',
+        'views/gamification_view.xml',
+        'views/hr_employee_view.xml',
+        'views/gamification_templates.xml',
     ],
     'auto_install': True,
 }

cogitoweb/OCB

addons/hr_gamification/__openerp__.py
@@ -15,9 +15,10 @@ Badge received are displayed on the user profile.
     'data': [
         'security/ir.model.access.csv',
         'security/gamification_security.xml',
-        'wizard/grant_badge.xml',
-        'views/gamification.xml',
-        'views/hr_gamification.xml',
+        'wizard/gamification_badge_user_wizard_views.xml',
+        'views/gamification_views.xml',
+        'views/hr_employee_views.xml',
+        'views/gamification_templates.xml',
     ],
     'auto_install': True,
 }