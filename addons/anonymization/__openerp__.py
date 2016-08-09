TheNiceGuy/odoo

[MOV] anonymization: Re-organized file structure

addons/anonymization/__openerp__.py
@@ -19,11 +19,11 @@ team. Once you get back your migrated database, you restore it and reverse the
 anonymization process to recover your previous data.
     """,
     'depends': ['base'],
-    'demo': ['anonymization_demo.xml'],
+    'demo': ['data/anonymization_demo.xml'],
     'data': [
-        'ir.model.fields.anonymization.csv',
+        'data/ir.model.fields.anonymization.csv',
         'security/ir.model.access.csv',
-        'anonymization_view.xml',
+        'views/anonymization_view.xml',
     ],
     'installable': True,
     'auto_install': False,

cogitoweb/OCB

addons/anonymization/__openerp__.py
@@ -19,11 +19,11 @@ team. Once you get back your migrated database, you restore it and reverse the
 anonymization process to recover your previous data.
     """,
     'depends': ['base'],
-    'demo': ['anonymization_demo.xml'],
+    'demo': ['data/anonymization_demo.xml'],
     'data': [
-        'ir.model.fields.anonymization.csv',
+        'data/ir.model.fields.anonymization.csv',
         'security/ir.model.access.csv',
-        'anonymization_view.xml',
+        'views/anonymization_views.xml',
     ],
     'installable': True,
     'auto_install': False,