beronoel/odoo

[IMP] all: clean usage of `safe_eval`
Specifically: do not import `safe_eval` as `eval`, keep it explicit instead.

addons/delivery/models/delivery_carrier.py
@@ -4,7 +4,7 @@
 import logging
 from openerp import api, fields, models, _
 from openerp.exceptions import UserError, ValidationError
-from openerp.tools.safe_eval import safe_eval as eval
+from openerp.tools.safe_eval import safe_eval
 
 _logger = logging.getLogger(__name__)
 
@@ -286,7 +286,7 @@ class DeliveryCarrier(models.Model):
         criteria_found = False
         price_dict = {'price': total, 'volume': volume, 'weight': weight, 'wv': volume * weight, 'quantity': quantity}
         for line in self.price_rule_ids:
-            test = eval(line.variable + line.operator + str(line.max_value), price_dict)
+            test = safe_eval(line.variable + line.operator + str(line.max_value), price_dict)
             if test:
                 price = line.list_base_price + line.list_price * price_dict[line.variable_factor]
                 criteria_found = True

addons/mail/models/mail_alias.py
@@ -8,7 +8,7 @@ from openerp import _, api, fields, models, SUPERUSER_ID
 from openerp.exceptions import ValidationError
 from openerp.modules.registry import RegistryManager
 from openerp.tools import ustr
-from openerp.tools.safe_eval import safe_eval as eval
+from openerp.tools.safe_eval import safe_eval
 
 _logger = logging.getLogger(__name__)
 
@@ -94,7 +94,7 @@ class Alias(models.Model):
     @api.constrains('alias_defaults')
     def _check_alias_defaults(self):
         try:
-            dict(eval(self.alias_defaults))
+            dict(safe_eval(self.alias_defaults))
         except Exception:
             raise ValidationError(_('Invalid expression, it must be a literal python dictionary definition e.g. "{\'field\': \'value\'}"'))
 

addons/mail/models/mail_channel.py
@@ -9,7 +9,7 @@ from openerp import _, api, fields, models, modules, tools
 from openerp.exceptions import UserError
 from openerp.osv import expression
 from openerp.tools import ormcache
-from openerp.tools.safe_eval import safe_eval as eval
+from openerp.tools.safe_eval import safe_eval
 
 
 
@@ -184,7 +184,7 @@ class Channel(models.Model):
         headers = {}
         if res.get('headers'):
             try:
-                headers.update(eval(res['headers']))
+                headers.update(safe_eval(res['headers']))
             except Exception:
                 pass
         headers['Precedence'] = 'list'

addons/mail/models/mail_thread.py
@@ -23,7 +23,7 @@ from openerp import _, api, fields, models
 from openerp import exceptions
 from openerp import tools
 from openerp.addons.mail.models.mail_message import decode
-from openerp.tools.safe_eval import safe_eval as eval
+from openerp.tools.safe_eval import safe_eval
 
 
 _logger = logging.getLogger(__name__)
@@ -372,7 +372,7 @@ class MailThread(models.AbstractModel):
             doc = etree.XML(res['arch'])
             for node in doc.xpath("//field[@name='message_ids']"):
                 # the 'Log a note' button is employee only
-                options = eval(node.get('options', '{}'))
+                options = safe_eval(node.get('options', '{}'))
                 is_employee = self.env.user.has_group('base.group_user')
                 options['display_log_button'] = is_employee
                 # emoji list
@@ -1102,7 +1102,7 @@ class MailThread(models.AbstractModel):
                         # user_id = self._message_find_user_id(cr, uid, message, context=context)
                         user_id = self._uid
                         _logger.info('No matching user_id for the alias %s', alias.alias_name)
-                    route = (alias.alias_model_id.model, alias.alias_force_thread_id, eval(alias.alias_defaults), user_id, alias)
+                    route = (alias.alias_model_id.model, alias.alias_force_thread_id, safe_eval(alias.alias_defaults), user_id, alias)
                     route = self.message_route_verify(
                         message, message_dict, route,
                         update_author=True, assert_model=True, create_fallback=True)

addons/mail/wizard/mail_compose_message.py
@@ -5,7 +5,7 @@ import re
 
 from openerp import _, api, fields, models, SUPERUSER_ID
 from openerp import tools
-from openerp.tools.safe_eval import safe_eval as eval
+from openerp.tools.safe_eval import safe_eval
 
 
 # main mako-like expression pattern
@@ -230,7 +230,7 @@ class MailComposer(models.TransientModel):
                 ActiveModel = ActiveModel.with_context(mail_notify_force_send=False, mail_create_nosubscribe=True)
             # wizard works in batch mode: [res_id] or active_ids or active_domain
             if mass_mode and wizard.use_active_domain and wizard.model:
-                res_ids = self.env[wizard.model].search(eval(wizard.active_domain)).ids
+                res_ids = self.env[wizard.model].search(safe_eval(wizard.active_domain)).ids
             elif mass_mode and wizard.model and self._context.get('active_ids'):
                 res_ids = self._context['active_ids']
             else:

cogitoweb/OCB

addons/delivery/models/delivery_carrier.py
@@ -5,7 +5,7 @@ import logging
 
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError, ValidationError
-from odoo.tools.safe_eval import safe_eval as eval
+from odoo.tools.safe_eval import safe_eval
 
 _logger = logging.getLogger(__name__)
 
@@ -287,7 +287,7 @@ class DeliveryCarrier(models.Model):
         criteria_found = False
         price_dict = {'price': total, 'volume': volume, 'weight': weight, 'wv': volume * weight, 'quantity': quantity}
         for line in self.price_rule_ids:
-            test = eval(line.variable + line.operator + str(line.max_value), price_dict)
+            test = safe_eval(line.variable + line.operator + str(line.max_value), price_dict)
             if test:
                 price = line.list_base_price + line.list_price * price_dict[line.variable_factor]
                 criteria_found = True

addons/mail/models/mail_alias.py
@@ -8,7 +8,7 @@ import unicodedata
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 from odoo.tools import ustr
-from odoo.tools.safe_eval import safe_eval as eval
+from odoo.tools.safe_eval import safe_eval
 
 _logger = logging.getLogger(__name__)
 
@@ -94,7 +94,7 @@ class Alias(models.Model):
     @api.constrains('alias_defaults')
     def _check_alias_defaults(self):
         try:
-            dict(eval(self.alias_defaults))
+            dict(safe_eval(self.alias_defaults))
         except Exception:
             raise ValidationError(_('Invalid expression, it must be a literal python dictionary definition e.g. "{\'field\': \'value\'}"'))
 

addons/mail/models/mail_channel.py
@@ -10,7 +10,7 @@ from odoo import _, api, fields, models, modules, tools
 from odoo.exceptions import UserError
 from odoo.osv import expression
 from odoo.tools import ormcache
-from odoo.tools.safe_eval import safe_eval as eval
+from odoo.tools.safe_eval import safe_eval
 
 
 class ChannelPartner(models.Model):
@@ -184,7 +184,7 @@ class Channel(models.Model):
         headers = {}
         if res.get('headers'):
             try:
-                headers.update(eval(res['headers']))
+                headers.update(safe_eval(res['headers']))
             except Exception:
                 pass
         headers['Precedence'] = 'list'

addons/mail/models/mail_thread.py
@@ -22,7 +22,7 @@ from werkzeug import url_encode
 
 from odoo import _, api, exceptions, fields, models, tools
 from odoo.addons.mail.models.mail_message import decode
-from odoo.tools.safe_eval import safe_eval as eval
+from odoo.tools.safe_eval import safe_eval
 
 
 _logger = logging.getLogger(__name__)
@@ -371,7 +371,7 @@ class MailThread(models.AbstractModel):
             doc = etree.XML(res['arch'])
             for node in doc.xpath("//field[@name='message_ids']"):
                 # the 'Log a note' button is employee only
-                options = eval(node.get('options', '{}'))
+                options = safe_eval(node.get('options', '{}'))
                 is_employee = self.env.user.has_group('base.group_user')
                 options['display_log_button'] = is_employee
                 # emoji list
@@ -1092,7 +1092,7 @@ class MailThread(models.AbstractModel):
                         # user_id = self._message_find_user_id(message)
                         user_id = self._uid
                         _logger.info('No matching user_id for the alias %s', alias.alias_name)
-                    route = (alias.alias_model_id.model, alias.alias_force_thread_id, eval(alias.alias_defaults), user_id, alias)
+                    route = (alias.alias_model_id.model, alias.alias_force_thread_id, safe_eval(alias.alias_defaults), user_id, alias)
                     route = self.message_route_verify(
                         message, message_dict, route,
                         update_author=True, assert_model=True, create_fallback=True)

addons/mail/wizard/mail_compose_message.py
@@ -5,7 +5,7 @@ import base64
 import re
 
 from odoo import _, api, fields, models, SUPERUSER_ID, tools
-from odoo.tools.safe_eval import safe_eval as eval
+from odoo.tools.safe_eval import safe_eval
 
 
 # main mako-like expression pattern
@@ -230,7 +230,7 @@ class MailComposer(models.TransientModel):
                 ActiveModel = ActiveModel.with_context(mail_notify_force_send=False, mail_create_nosubscribe=True)
             # wizard works in batch mode: [res_id] or active_ids or active_domain
             if mass_mode and wizard.use_active_domain and wizard.model:
-                res_ids = self.env[wizard.model].search(eval(wizard.active_domain)).ids
+                res_ids = self.env[wizard.model].search(safe_eval(wizard.active_domain)).ids
             elif mass_mode and wizard.model and self._context.get('active_ids'):
                 res_ids = self._context['active_ids']
             else: