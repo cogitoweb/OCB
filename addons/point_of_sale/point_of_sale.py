cogitoweb/OCB

commit 808fd1de89bc11099508bf0636b23e829d0c12d1

[FIX] point_of_sale: set pricelist in create
The pricelist is set by default as the one of the first opened session. It's not
necessary correct (if more than one session) and is not working for rescue
sessions. Always set one.

addons/point_of_sale/point_of_sale.py
@@ -763,6 +763,7 @@ class pos_order(osv.osv):
             # set name based on the sequence specified on the config
             session = self.pool['pos.session'].browse(cr, uid, values['session_id'], context=context)
             values['name'] = session.config_id.sequence_id._next()
+            values.setdefault('session_id', session.config_id.pricelist_id.id)
         else:
             # fallback on any pos.order sequence
             values['name'] = self.pool.get('ir.sequence').next_by_code(cr, uid, 'pos.order', context=context)

commit d68bf7836ff92ca2e9531cc54469353cb459a75f

[FIX] point_of_sale: correct typo in order pricelist selection
Typo introduced in 808fd1de89bc11099508bf0636b23e829d0c12d1
prevented the selection of the correct non-default pricelist.

addons/point_of_sale/point_of_sale.py
@@ -932,7 +932,7 @@ class pos_order(osv.osv):
             # set name based on the sequence specified on the config
             session = self.pool['pos.session'].browse(cr, uid, values['session_id'], context=context)
             values['name'] = session.config_id.sequence_id._next()
-            values.setdefault('session_id', session.config_id.pricelist_id.id)
+            values.setdefault('pricelist_id', session.config_id.pricelist_id.id)
         else:
             # fallback on any pos.order sequence
             values['name'] = self.pool.get('ir.sequence').next_by_code(cr, uid, 'pos.order', context=context)