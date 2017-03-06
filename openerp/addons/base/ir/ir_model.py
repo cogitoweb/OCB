cogitoweb/OCB

commit 91b9649b758a2e96bfd7e1216643c6d0dfb88b98

[FIX] base: prevent deletion of custom fields when other fields depend on them (#13978)
Consider that you have a custom one2many field based on a custom many2one
field, and that you delete the many2one field.  From that point on, it is
impossible to load the registry of the corresponding database.  To prevent this
from happening, we add a check before modifying or deleting custom fields.

openerp/addons/base/ir/ir_model.py
@@ -142,6 +142,10 @@ class ir_model(osv.osv):
                 if model.state != 'manual':
                     raise UserError(_("Model '%s' contains module data and cannot be removed!") % (model.name,))
 
+        # prevent screwing up fields that depend on these models' fields
+        for model in self.browse(cr, user, ids, context=context):
+            model.field_id._prepare_update()
+
         self._drop_table(cr, user, ids, context)
         res = super(ir_model, self).unlink(cr, user, ids, context)
         if not context.get(MODULE_UNINSTALL_FLAG):
@@ -431,6 +435,19 @@ class ir_model_fields(osv.osv):
 
         return True
 
+    @api.multi
+    def _prepare_update(self):
+        """ Check whether the fields in ``self`` may be modified or removed.
+            This method prevents the modification/deletion of many2one fields
+            that have an inverse one2many, for instance.
+        """
+        for record in self:
+            model = self.env[record.model]
+            field = model._fields[record.name]
+            if field.type == 'many2one' and model._field_inverses.get(field):
+                msg = _("The field '%s' cannot be removed because the field '%s' depends on it.")
+                raise UserError(msg % (field, model._field_inverses[field][0]))
+
     def unlink(self, cr, user, ids, context=None):
         # Prevent manual deletion of module columns
         if context is None: context = {}
@@ -439,6 +456,9 @@ class ir_model_fields(osv.osv):
         if not context.get(MODULE_UNINSTALL_FLAG) and \
                 any(field.state != 'manual' for field in self.browse(cr, user, ids, context)):
             raise UserError(_("This column contains module data and cannot be removed!"))
+
+        # prevent screwing up fields that depend on these fields
+        self.browse(cr, user, ids, context=context)._prepare_update()
 
         self._drop_column(cr, user, ids, context)
         res = super(ir_model_fields, self).unlink(cr, user, ids, context)
@@ -528,6 +548,7 @@ class ir_model_fields(osv.osv):
 
                 if vals.get('name', item.name) != item.name:
                     # We need to rename the column
+                    item._prepare_update()
                     if column_rename:
                         raise UserError(_('Can only rename one field at a time!'))
                     if vals['name'] in obj._fields:

commit 5a38fb2df8d7ba67a139f07b205c5e1e861a8a3e

[FIX] uninstall: unlink of many2one fields having one2many dependencies
The revision
91b9649b758a2e96bfd7e1216643c6d0dfb88b98
introduced a regression:
many2one fields having one2many depending on it declared before
the declaration of the many2one itself were not unlinked
during a module uninstall.

As a result, the column in the table was not dropped,
and the table itself was not deleted either, not even truncated.

Therefore, when you tried to reinstall the module, you had data
left.

The check of the one2many dependencies must not be performed
during an uninstall. Anyway, this wasn't blocking the uninstall
of the modules,
it was just preventing the field  to be deleted, that's all.

opw-710291

openerp/addons/base/ir/ir_model.py
@@ -141,10 +141,8 @@ class ir_model(osv.osv):
             for model in self.browse(cr, user, ids, context):
                 if model.state != 'manual':
                     raise UserError(_("Model '%s' contains module data and cannot be removed!") % (model.name,))
-
-        # prevent screwing up fields that depend on these models' fields
-        for model in self.browse(cr, user, ids, context=context):
-            model.field_id._prepare_update()
+                # prevent screwing up fields that depend on these models' fields
+                model.field_id._prepare_update()
 
         self._drop_table(cr, user, ids, context)
         res = super(ir_model, self).unlink(cr, user, ids, context)