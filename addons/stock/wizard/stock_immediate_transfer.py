pledra/odoo

[IMP] stock: make immediate transfer and backorder multi
The purpose of this commit is to allow future modules that need
immediate transfer and back order function on multiples picking
to do it.

This commit does not modify the behavior of single picking processing

addons/stock/wizard/stock_immediate_transfer.py
@@ -9,31 +9,38 @@ class StockImmediateTransfer(models.TransientModel):
     _name = 'stock.immediate.transfer'
     _description = 'Immediate Transfer'
 
-    pick_id = fields.Many2one('stock.picking')
+    pick_ids = fields.Many2many('stock.picking', 'stock_picking_transfer_rel')
 
     @api.model
     def default_get(self, fields):
         res = super(StockImmediateTransfer, self).default_get(fields)
-        if not res.get('pick_id') and self._context.get('active_id'):
-            res['pick_id'] = self._context['active_id']
+        if not res.get('pick_ids') and self._context.get('active_id'):
+            res['pick_ids'] = self._context['active_id']
         return res
 
     @api.multi
     def process(self):
-        self.ensure_one()
-        # If still in draft => confirm and assign
-        if self.pick_id.state == 'draft':
-            self.pick_id.action_confirm()
-            if self.pick_id.state != 'assigned':
-                self.pick_id.action_assign()
-                if self.pick_id.state != 'assigned':
-                    raise UserError(_("Could not reserve all requested products. Please use the \'Mark as Todo\' button to handle the reservation manually."))
-        for move in self.pick_id.move_lines:
-            if move.move_line_ids:
-                for move_line in move.move_line_ids:
-                    move_line.qty_done = move_line.product_uom_qty
-            else:
-                move.quantity_done = move.product_uom_qty
-        if self.pick_id.check_backorder():
-            return self.pick_id.action_generate_backorder_wizard()
-        self.pick_id.do_transfer()
+        pick_to_backorder = self.env['stock.picking']
+        pick_to_do = self.env['stock.picking']
+        for picking in self.pick_ids:
+            # If still in draft => confirm and assign
+            if picking.state == 'draft':
+                picking.action_confirm()
+                if picking.state != 'assigned':
+                    picking.action_assign()
+                    if picking.state != 'assigned':
+                        raise UserError(_("Could not reserve all requested products. Please use the \'Mark as Todo\' button to handle the reservation manually."))
+            for move in picking.move_lines:
+                if move.move_line_ids:
+                    for move_line in move.move_line_ids:
+                        move_line.qty_done = move_line.product_uom_qty
+                else:
+                    move.quantity_done = move.product_uom_qty
+            if picking.check_backorder():
+                pick_to_backorder |= picking
+                continue
+            pick_to_do |= picking
+        if pick_to_do:
+            pick_to_do.action_done()
+        if pick_to_backorder:
+            return pick_to_backorder.action_generate_backorder_wizard()

cogitoweb/OCB

addons/stock/wizard/stock_immediate_transfer.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models, _
+from odoo import fields, models, _
 from odoo.exceptions import UserError
 
 
@@ -9,30 +9,31 @@ class StockImmediateTransfer(models.TransientModel):
     _name = 'stock.immediate.transfer'
     _description = 'Immediate Transfer'
 
-    pick_id = fields.Many2one('stock.picking')
-
-    @api.model
-    def default_get(self, fields):
-        res = super(StockImmediateTransfer, self).default_get(fields)
-        if not res.get('pick_id') and self._context.get('active_id'):
-            res['pick_id'] = self._context['active_id']
-        return res
+    pick_ids = fields.Many2many('stock.picking', 'stock_picking_transfer_rel')
 
     def process(self):
-        self.ensure_one()
-        # If still in draft => confirm and assign
-        if self.pick_id.state == 'draft':
-            self.pick_id.action_confirm()
-            if self.pick_id.state != 'assigned':
-                self.pick_id.action_assign()
-                if self.pick_id.state != 'assigned':
-                    raise UserError(_("Could not reserve all requested products. Please use the \'Mark as Todo\' button to handle the reservation manually."))
-        for move in self.pick_id.move_lines:
-            if move.move_line_ids:
-                for move_line in move.move_line_ids:
-                    move_line.qty_done = move_line.product_uom_qty
-            else:
-                move.quantity_done = move.product_uom_qty
-        if self.pick_id._check_backorder():
-            return self.pick_id.action_generate_backorder_wizard()
-        self.pick_id.do_transfer()
+        pick_to_backorder = self.env['stock.picking']
+        pick_to_do = self.env['stock.picking']
+        for picking in self.pick_ids:
+            # If still in draft => confirm and assign
+            if picking.state == 'draft':
+                picking.action_confirm()
+                if picking.state != 'assigned':
+                    picking.action_assign()
+                    if picking.state != 'assigned':
+                        raise UserError(_("Could not reserve all requested products. Please use the \'Mark as Todo\' button to handle the reservation manually."))
+            for move in picking.move_lines:
+                if move.move_line_ids:
+                    for move_line in move.move_line_ids:
+                        move_line.qty_done = move_line.product_uom_qty
+                else:
+                    move.quantity_done = move.product_uom_qty
+            if picking._check_backorder():
+                pick_to_backorder |= picking
+                continue
+            pick_to_do |= picking
+        # Process every picking that do not require a backorder, then return a single backorder wizard for every other ones.
+        if pick_to_do:
+            pick_to_do.action_done()
+        if pick_to_backorder:
+            return pick_to_backorder.action_generate_backorder_wizard()