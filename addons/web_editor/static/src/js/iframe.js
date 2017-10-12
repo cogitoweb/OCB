cogitoweb/OCB

commit 61cb0ca722e285abf9f24509fb5a2cbfc3afe17c

[REF] web_editor, website, *: complete refactoring
* mass_mailing, payment, point_of_sale, portal, survey, web, web_tour,
  website_blog, website_crm_partner_assign, website_event,
  website_event_questions, website_form, website_forum, website_gengo,
  website_hr_recruitment, website_links, website_mail,
  website_mail_channel, website_mass_mailing, website_quote,
  website_sale, website_sale_options, website_slides, website_twitter

This commit reviews the whole "JS side" of the web_editor and website
apps. This is a first step to be able to improve them with new and
better functionnalities; this commit is not supposed to change any
visual behavior.

The main goal was to achieve a structure similar to the backend one.
Now, the frontend side also has a root widget (like the WebClient)
and all other widgets are attached to it one way or another. This allows
the benefits of using the 'trigger_up' functionnality for example.

As RPC are now mainly done with the `this._rpc` functionnality (being
possible thanks to the parent hierarchy), the frontend will also be
possible to test thanks to QUnit in a future update (besides the "text"
editor side which still requires a refactoring to be able to do that).

---

Here are some of the changes:

(-) conventions and documentation

The code has been updated to follow JS conventions and a lot of code has
been commented (around +2000 lines of comment). This also means that
lots of functions have been renamed to use camelCase or simply to make
their name understandable.
See https://github.com/odoo/odoo/wiki/Javascript-coding-guidelines.

(-) deprecated: web_editor.base

The "web_editor.base" module has been split and does not force the
modules which require it to wait for DOM ready anymore. This was indeed
slowing loading times, but also prevented to use some modules in some
contexts (see the LESS editor use in web_studio which is the subject of
another task).
Now the "editor context" can be got thanks to the "web_editor.context"

addons/web_editor/static/src/js/iframe.js
@@ -1,29 +1,29 @@
 odoo.define('web_editor.iframe', function (require) {
 'use strict';
 
-var ajax = require("web.ajax");
-var core = require("web.core");
+var core = require('web.core');
 var editor = require('web_editor.editor');
 var translator = require('web_editor.translate');
 var rte = require('web_editor.rte');
-var snippet_editor = require('web_editor.snippet.editor');
 
-var callback = window ? window["callback"] : undefined;
-window.top.odoo[callback+"_updown"] = function (value, fields_values, field_name) {
-    var $editable = $("#editable_area");
-    if(value !== $editable.prop("innerHTML")) {
-        if ($('body').hasClass('editor_enable')) {
-            if (value !== fields_values[field_name]) {
-                rte.history.recordUndo($editable);
-            }
-            snippet_editor.instance.make_active(false);
+var callback = window ? window['callback'] : undefined;
+window.top.odoo[callback + '_updown'] = function (value, fields_values, field_name) {
+    var $editable = $('#editable_area');
+    if (value === $editable.prop('innerHTML')) {
+        return;
+    }
+
+    if ($('body').hasClass('editor_enable')) {
+        if (value !== fields_values[field_name]) {
+            rte.history.recordUndo($editable);
         }
+        core.bus.trigger('deactivate_snippet');
+    }
 
-        $editable.html(value);
+    $editable.html(value);
 
-        if ($('body').hasClass('editor_enable') && value !== fields_values[field_name]) {
-            $editable.trigger("content_changed");
-        }
+    if ($('body').hasClass('editor_enable') && value !== fields_values[field_name]) {
+        $editable.trigger('content_changed');
     }
 };
 
@@ -32,33 +32,26 @@ editor.Class.include({
         this.on('rte:start', this, function () {
             this.$('form').hide();
 
-            if (window.top.odoo[callback+"_editor"]) {
-                window.top.odoo[callback+"_editor"](this);
+            if (window.top.odoo[callback + '_editor']) {
+                window.top.odoo[callback + '_editor'](this);
             }
 
-            var $editable = $("#editable_area");
+            var $editable = $('#editable_area');
             setTimeout(function () {
-                $($editable.find("*").filter(function () {return !this.children.length;}).first()[0] || $editable)
-                    .focusIn().trigger("mousedown").trigger("keyup");
+                $($editable.find('*').filter(function () {return !this.children.length;}).first()[0] || $editable)
+                    .focusIn().trigger('mousedown').trigger('keyup');
             },0);
 
             $editable.on('content_changed', this, function () {
-                if (window.top.odoo[callback+"_downup"]) {
-                    window.top.odoo[callback+"_downup"]($editable.prop('innerHTML'));
+                if (window.top.odoo[callback + '_downup']) {
+                    window.top.odoo[callback + '_downup']($editable.prop('innerHTML'));
                 }
             });
         });
 
-        this.on("snippets:ready", this, function () {
-            $(window.top).trigger("resize");
+        return this._super.apply(this, arguments).then(function () {
+            $(window.top).trigger('resize'); // TODO check, probably useless
         });
-        return this._super.apply(this, arguments);
-    }
-});
-
-snippet_editor.Class.include({
-    _get_snippet_url: function () {
-        return snippets_url;
     }
 });
 
@@ -69,18 +62,89 @@ rte.Class.include({
             config.airPopover.splice(7, 0, ['view', ['codeview']]);
         }
         return config;
-    }
+    },
 });
 
 translator.Class.include({
     start: function () {
         var res = this._super.apply(this, arguments);
         $('button[data-action=save]').hide();
-        if (window.top.odoo[callback+"_editor"]) {
-            window.top.odoo[callback+"_editor"](this);
+        if (window.top.odoo[callback + '_editor']) {
+            window.top.odoo[callback + '_editor'](this);
         }
         return res;
-    }
+    },
+});
 });
 
+//==============================================================================
+
+odoo.define('web_editor.IframeRoot.instance', function (require) {
+'use strict';
+
+require('web.dom_ready');
+var iframeRootData = require('web_editor.IframeRoot');
+
+var iframeRoot = new iframeRootData.IframeRoot(null);
+return iframeRoot.attachTo(document.body).then(function () {
+    return iframeRoot;
+});
+});
+
+//==============================================================================
+
+odoo.define('web_editor.IframeRoot', function (require) {
+'use strict';
+
+var BodyManager = require('web_editor.BodyManager');
+var weContext = require('web_editor.context');
+var editor = require('web_editor.editor');
+var rootWidget = require('web_editor.root_widget');
+var translate = require('web_editor.translate');
+
+var iframeRootRegistry = new rootWidget.RootWidgetRegistry();
+
+var IframeRoot = BodyManager.extend({
+    /**
+     * @override
+     * @todo this is somehow a duplicate of website features
+     */
+    start: function () {
+        var defs = [this._super.apply(this, arguments)];
+
+        var ctx = weContext.getExtra();
+
+        if (ctx.editable && window.location.search.indexOf('enable_editor') >= 0) {
+            var editorInstance = new (editor.Class)(this);
+            defs.push(editorInstance.prependTo(this.$el));
+        }
+
+        if (ctx.edit_translations) {
+            var translator = new (translate.Class)(this, this.$('#wrapwrap'));
+            defs.push(translator.prependTo(this.$el));
+        }
+
+        return $.when.apply($, defs);
+    },
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * As the IframeRoot instance is designed to be unique, the associated
+     * registry has been instantiated outside of the class and is simply
+     * returned here.
+     *
+     * @override
+     */
+    _getRegistry: function () {
+        return iframeRootRegistry;
+    },
+});
+
+return {
+    IframeRoot: IframeRoot,
+    iframeRootRegistry: iframeRootRegistry,
+};
 });

commit 93785d35eeda10fe244ff9e21e35780d11a65680

[FIX] web_editor: restore iframe editor code view mode
Broken since https://github.com/odoo/odoo/commit/61cb0ca722e285abf9f24509fb5a2cbfc3afe17c

addons/web_editor/static/src/js/iframe.js
@@ -56,7 +56,10 @@ editor.Class.include({
 });
 
 rte.Class.include({
-    config: function ($editable) {
+    /**
+     * @override
+     */
+    _getDefaultConfig: function ($editable) {
         var config = this._super.apply(this, arguments);
         if ($.deparam($.param.querystring()).debug !== undefined) {
             config.airPopover.splice(7, 0, ['view', ['codeview']]);