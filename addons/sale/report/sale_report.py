pledra/odoo

[IMP] sale: better amount to invoice and invoiced computation
Since quantity to invoice and quantity invoiced
already take into account refund invoices, the
calculation of those amounts can be simplify.
Also, we simply need those on the sale.report,
so removing them from sale.order.line model
will ease performance.

addons/sale/report/sale_report.py
@@ -26,8 +26,8 @@ class SaleReport(models.Model):
     user_id = fields.Many2one('res.users', 'Salesperson', readonly=True)
     price_total = fields.Float('Total', readonly=True)
     price_subtotal = fields.Float('Untaxed Total', readonly=True)
-    amt_to_invoice = fields.Float('Amount To Invoice', readonly=True)
-    amt_invoiced = fields.Float('Amount Invoiced', readonly=True)
+    amout_to_invoice = fields.Float('Amount To Invoice', readonly=True)
+    amout_invoiced = fields.Float('Amount Invoiced', readonly=True)
     product_tmpl_id = fields.Many2one('product.template', 'Product Template', readonly=True)
     categ_id = fields.Many2one('product.category', 'Product Category', readonly=True)
     nbr = fields.Integer('# of Lines', readonly=True)
@@ -58,8 +58,8 @@ class SaleReport(models.Model):
                     sum(l.qty_to_invoice / u.factor * u2.factor) as qty_to_invoice,
                     sum(l.price_total / COALESCE(cr.rate, 1.0)) as price_total,
                     sum(l.price_subtotal / COALESCE(cr.rate, 1.0)) as price_subtotal,
-                    sum(l.amt_to_invoice / COALESCE(cr.rate, 1.0)) as amt_to_invoice,
-                    sum(l.amt_invoiced / COALESCE(cr.rate, 1.0)) as amt_invoiced,
+                    sum(l.price_reduce * l.qty_to_invoice / COALESCE(cr.rate, 1.0)) as amout_to_invoice,
+                    sum(l.price_reduce * l.qty_invoiced / COALESCE(cr.rate, 1.0)) as amout_invoiced,
                     count(*) as nbr,
                     s.name as name,
                     s.date_order as date,

cogitoweb/OCB

addons/sale/report/sale_report.py
@@ -26,8 +26,8 @@ class SaleReport(models.Model):
     user_id = fields.Many2one('res.users', 'Salesperson', readonly=True)
     price_total = fields.Float('Total', readonly=True)
     price_subtotal = fields.Float('Untaxed Total', readonly=True)
-    amt_to_invoice = fields.Float('Amount To Invoice', readonly=True)
-    amt_invoiced = fields.Float('Amount Invoiced', readonly=True)
+    amount_to_invoice = fields.Float('Amount To Invoice', readonly=True)
+    amount_invoiced = fields.Float('Amount Invoiced', readonly=True)
     product_tmpl_id = fields.Many2one('product.template', 'Product Template', readonly=True)
     categ_id = fields.Many2one('product.category', 'Product Category', readonly=True)
     nbr = fields.Integer('# of Lines', readonly=True)
@@ -58,8 +58,8 @@ class SaleReport(models.Model):
                     sum(l.qty_to_invoice / u.factor * u2.factor) as qty_to_invoice,
                     sum(l.price_total / COALESCE(cr.rate, 1.0)) as price_total,
                     sum(l.price_subtotal / COALESCE(cr.rate, 1.0)) as price_subtotal,
-                    sum(l.amt_to_invoice / COALESCE(cr.rate, 1.0)) as amt_to_invoice,
-                    sum(l.amt_invoiced / COALESCE(cr.rate, 1.0)) as amt_invoiced,
+                    sum(l.price_reduce * l.qty_to_invoice / COALESCE(cr.rate, 1.0)) as amount_to_invoice,
+                    sum(l.price_reduce * l.qty_invoiced / COALESCE(cr.rate, 1.0)) as amount_invoiced,
                     count(*) as nbr,
                     s.name as name,
                     s.date_order as date,