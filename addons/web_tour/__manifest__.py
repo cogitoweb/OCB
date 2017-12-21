pledra/odoo

[IMP] web_tour: Disable tours when Odoo installed with demo data

addons/web_tour/__manifest__.py
@@ -16,6 +16,9 @@ Odoo Web tours.
         'views/tour_templates.xml',
         'views/tour_views.xml'
     ],
+    'demo': [
+        'data/web_tour_data.xml',
+    ],
     'qweb': [
         "static/src/xml/*.xml",
     ],

cogitoweb/OCB

addons/web_tour/__manifest__.py
@@ -16,6 +16,9 @@ Odoo Web tours.
         'views/tour_templates.xml',
         'views/tour_views.xml'
     ],
+    'demo': [
+        'data/web_tour_demo.xml',
+    ],
     'qweb': [
         "static/src/xml/*.xml",
     ],