pledra/odoo

[IMP] hr_expense: make test smarter
This commit make expense test depending on the
same common class as the other (in sale_expense).

Some data were badly defined (test data), using
field only existing when sale is installed. So move
this part in sale_expense.

Expense tests now check analytic entries when
sumbitting an expense.

addons/hr_expense/tests/test_expenses.py
@@ -1,89 +1,62 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo.tests.common import TransactionCase
+from odoo.addons.hr_expense.tests.common import CommonTest
 
 
-class TestCheckJournalEntry(TransactionCase):
+class TestAccountEntry(CommonTest):
     """
     Check journal entries when the expense product is having tax which is tax included.
     """
 
     def setUp(self):
-        super(TestCheckJournalEntry, self).setUp()
+        super(TestAccountEntry, self).setUp()
+        self.product_deliver_cost.write({'supplier_taxes_id': [(6, 0, [self.tax.id])]})
 
-        self.tax = self.env['account.tax'].create({
-            'name': 'Expense 10%',
-            'amount': 10,
-            'amount_type': 'percent',
-            'type_tax_use': 'purchase',
-            'price_include': True,
-        })
-        self.product = self.env.ref('hr_expense.air_ticket')
-        self.product.write({'supplier_taxes_id': [(6, 0, [self.tax.id])]})
-
-        self.employee = self.env.ref('hr.employee_mit')
-
-        # Create payable account for the expense
-        user_type = self.env.ref('account.data_account_type_payable')
-        account_payable = self.env['account.account'].create({
-            'code': 'X1111',
-            'name': 'HR Expense - Test Payable Account',
-            'user_type_id': user_type.id,
-            'reconcile': True
-        })
-        self.employee.address_home_id.property_account_payable_id = account_payable.id
-
-        # Create expenses account for the expense
-        user_type = self.env.ref('account.data_account_type_expenses')
-        account_expense = self.env['account.account'].create({
-            'code': 'X2120',
-            'name': 'HR Expense - Test Purchase Account',
-            'user_type_id': user_type.id
-        })
-        # Assign it to the air ticket product
-        self.product.write({'property_account_expense_id': account_expense.id})
-
-        # Create Sales Journal
-        company = self.env.ref('base.main_company')
-        self.env['account.journal'].create({
-            'name': 'Purchase Journal - Test',
-            'code': 'HRTPJ',
-            'type': 'purchase',
-            'company_id': company.id
-        })
-
-        self.expense = self.env['hr.expense.sheet'].create({
+    def test_account_entry(self):
+        """ Checking accounting move entries and analytic entries when submitting expense """
+        expense = self.env['hr.expense.sheet'].create({
             'name': 'Expense for John Smith',
             'employee_id': self.employee.id,
         })
-        self.expense_line = self.env['hr.expense'].create({
+        expense_line = self.env['hr.expense'].create({
             'name': 'Car Travel Expenses',
             'employee_id': self.employee.id,
-            'product_id': self.product.id,
+            'product_id': self.product_deliver_cost.id,
             'unit_amount': 700.00,
             'tax_ids': [(6, 0, [self.tax.id])],
-            'sheet_id': self.expense.id,
+            'sheet_id': expense.id,
+            'analytic_account_id': self.analytic_account.id,
         })
-
-    def test_journal_entry(self):
+        expense_line._onchange_product_id()
         # Submitted to Manager
-        self.assertEquals(self.expense.state, 'submit', 'Expense is not in Reported state')
+        self.assertEquals(expense.state, 'submit', 'Expense is not in Reported state')
         # Approve
-        self.expense.approve_expense_sheets()
-        self.assertEquals(self.expense.state, 'approve', 'Expense is not in Approved state')
+        expense.approve_expense_sheets()
+        self.assertEquals(expense.state, 'approve', 'Expense is not in Approved state')
         # Create Expense Entries
-        self.expense.action_sheet_move_create()
-        self.assertEquals(self.expense.state, 'post', 'Expense is not in Waiting Payment state')
-        self.assertTrue(self.expense.account_move_id.id, 'Expense Journal Entry is not created')
+        expense.action_sheet_move_create()
+        self.assertEquals(expense.state, 'post', 'Expense is not in Waiting Payment state')
+        self.assertTrue(expense.account_move_id.id, 'Expense Journal Entry is not created')
 
         # [(line.debit, line.credit, line.tax_line_id.id) for line in self.expense.expense_line_ids.account_move_id.line_ids]
         # should git this result [(0.0, 700.0, False), (63.64, 0.0, 179), (636.36, 0.0, False)]
-        for line in self.expense.account_move_id.line_ids:
+        for line in expense.account_move_id.line_ids:
             if line.credit:
                 self.assertAlmostEquals(line.credit, 700.00)
+                self.assertEquals(len(line.analytic_line_ids), 0, "The credit move line should not have analytic lines")
+                self.assertFalse(line.product_id, "Product of credit move line should be false")
             else:
                 if not line.tax_line_id == self.tax:
                     self.assertAlmostEquals(line.debit, 636.36)
+                    self.assertEquals(len(line.analytic_line_ids), 1, "The debit move line should have 1 analytic lines")
+                    self.assertEquals(line.product_id, self.product_deliver_cost, "Product of debit move line should be the one from the expense")
                 else:
                     self.assertAlmostEquals(line.debit, 63.64)
+                    self.assertEquals(len(line.analytic_line_ids), 0, "The tax move line should not have analytic lines")
+                    self.assertFalse(line.product_id, "Product of tax move line should be false")
+
+        self.assertEquals(self.analytic_account.line_ids, expense.account_move_id.mapped('line_ids.analytic_line_ids'))
+        self.assertEquals(len(self.analytic_account.line_ids), 1, "Analytic Account should have only one line")
+        self.assertAlmostEquals(self.analytic_account.line_ids[0].amount, -636.36, "Amount on the only AAL is wrong")
+        self.assertEquals(self.analytic_account.line_ids[0].product_id, self.product_deliver_cost, "Product of AAL should be the one from the expense")

cogitoweb/OCB

addons/hr_expense/tests/test_expenses.py
@@ -1,89 +1,74 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo.tests.common import TransactionCase
+from odoo.addons.hr_expense.tests.common import TestExpenseCommon
 
 
-class TestCheckJournalEntry(TransactionCase):
+class TestAccountEntry(TestExpenseCommon):
     """
     Check journal entries when the expense product is having tax which is tax included.
     """
 
     def setUp(self):
-        super(TestCheckJournalEntry, self).setUp()
+        super(TestAccountEntry, self).setUp()
 
-        self.tax = self.env['account.tax'].create({
-            'name': 'Expense 10%',
-            'amount': 10,
-            'amount_type': 'percent',
-            'type_tax_use': 'purchase',
-            'price_include': True,
-        })
-        self.product = self.env.ref('hr_expense.air_ticket')
-        self.product.write({'supplier_taxes_id': [(6, 0, [self.tax.id])]})
+        self.setUpAdditionalAccounts()
 
-        self.employee = self.env.ref('hr.employee_mit')
-
-        # Create payable account for the expense
-        user_type = self.env.ref('account.data_account_type_payable')
-        account_payable = self.env['account.account'].create({
-            'code': 'X1111',
-            'name': 'HR Expense - Test Payable Account',
-            'user_type_id': user_type.id,
-            'reconcile': True
-        })
-        self.employee.address_home_id.property_account_payable_id = account_payable.id
-
-        # Create expenses account for the expense
-        user_type = self.env.ref('account.data_account_type_expenses')
-        account_expense = self.env['account.account'].create({
-            'code': 'X2120',
-            'name': 'HR Expense - Test Purchase Account',
-            'user_type_id': user_type.id
-        })
-        # Assign it to the air ticket product
-        self.product.write({'property_account_expense_id': account_expense.id})
-
-        # Create Sales Journal
-        company = self.env.ref('base.main_company')
-        self.env['account.journal'].create({
-            'name': 'Purchase Journal - Test',
-            'code': 'HRTPJ',
-            'type': 'purchase',
-            'company_id': company.id
+        self.product_expense = self.env['product.product'].create({
+            'name': "Delivered at cost",
+            'standard_price': 700,
+            'list_price': 700,
+            'type': 'consu',
+            'supplier_taxes_id': [(6, 0, [self.tax.id])],
+            'default_code': 'CONSU-DELI-COST',
+            'taxes_id': False,
+            'property_account_expense_id': self.account_expense.id,
         })
 
-        self.expense = self.env['hr.expense.sheet'].create({
+    def test_account_entry(self):
+        """ Checking accounting move entries and analytic entries when submitting expense """
+        expense = self.env['hr.expense.sheet'].create({
             'name': 'Expense for John Smith',
             'employee_id': self.employee.id,
         })
-        self.expense_line = self.env['hr.expense'].create({
+        expense_line = self.env['hr.expense'].create({
             'name': 'Car Travel Expenses',
             'employee_id': self.employee.id,
-            'product_id': self.product.id,
+            'product_id': self.product_expense.id,
             'unit_amount': 700.00,
             'tax_ids': [(6, 0, [self.tax.id])],
-            'sheet_id': self.expense.id,
+            'sheet_id': expense.id,
+            'analytic_account_id': self.analytic_account.id,
         })
-
-    def test_journal_entry(self):
+        expense_line._onchange_product_id()
         # Submitted to Manager
-        self.assertEquals(self.expense.state, 'submit', 'Expense is not in Reported state')
+        self.assertEquals(expense.state, 'submit', 'Expense is not in Reported state')
         # Approve
-        self.expense.approve_expense_sheets()
-        self.assertEquals(self.expense.state, 'approve', 'Expense is not in Approved state')
+        expense.approve_expense_sheets()
+        self.assertEquals(expense.state, 'approve', 'Expense is not in Approved state')
         # Create Expense Entries
-        self.expense.action_sheet_move_create()
-        self.assertEquals(self.expense.state, 'post', 'Expense is not in Waiting Payment state')
-        self.assertTrue(self.expense.account_move_id.id, 'Expense Journal Entry is not created')
+        expense.action_sheet_move_create()
+        self.assertEquals(expense.state, 'post', 'Expense is not in Waiting Payment state')
+        self.assertTrue(expense.account_move_id.id, 'Expense Journal Entry is not created')
 
         # [(line.debit, line.credit, line.tax_line_id.id) for line in self.expense.expense_line_ids.account_move_id.line_ids]
         # should git this result [(0.0, 700.0, False), (63.64, 0.0, 179), (636.36, 0.0, False)]
-        for line in self.expense.account_move_id.line_ids:
+        for line in expense.account_move_id.line_ids:
             if line.credit:
                 self.assertAlmostEquals(line.credit, 700.00)
+                self.assertEquals(len(line.analytic_line_ids), 0, "The credit move line should not have analytic lines")
+                self.assertFalse(line.product_id, "Product of credit move line should be false")
             else:
                 if not line.tax_line_id == self.tax:
                     self.assertAlmostEquals(line.debit, 636.36)
+                    self.assertEquals(len(line.analytic_line_ids), 1, "The debit move line should have 1 analytic lines")
+                    self.assertEquals(line.product_id, self.product_expense, "Product of debit move line should be the one from the expense")
                 else:
                     self.assertAlmostEquals(line.debit, 63.64)
+                    self.assertEquals(len(line.analytic_line_ids), 0, "The tax move line should not have analytic lines")
+                    self.assertFalse(line.product_id, "Product of tax move line should be false")
+
+        self.assertEquals(self.analytic_account.line_ids, expense.account_move_id.mapped('line_ids.analytic_line_ids'))
+        self.assertEquals(len(self.analytic_account.line_ids), 1, "Analytic Account should have only one line")
+        self.assertAlmostEquals(self.analytic_account.line_ids[0].amount, -636.36, "Amount on the only AAL is wrong")
+        self.assertEquals(self.analytic_account.line_ids[0].product_id, self.product_expense, "Product of AAL should be the one from the expense")