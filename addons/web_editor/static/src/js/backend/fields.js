marchon/odoo

[FIX] web_editor: add thumbnail image to send attachment

addons/web_editor/static/src/js/backend/fields.js
@@ -118,6 +118,7 @@ var FieldTextHtmlSimple = basic_fields.DebouncedField.extend(TranslatableFieldMi
      */
     _getValue: function () {
         if (this.nodeOptions['style-inline']) {
+            transcoder.attachmentThumbnailToLinkImg(this.$content);
             transcoder.classToStyle(this.$content);
             transcoder.fontToImg(this.$content);
         }

cogitoweb/OCB

addons/web_editor/static/src/js/backend/fields.js
@@ -118,6 +118,7 @@ var FieldTextHtmlSimple = basic_fields.DebouncedField.extend(TranslatableFieldMi
      */
     _getValue: function () {
         if (this.nodeOptions['style-inline']) {
+            transcoder.linkImgToAttachmentThumbnail(this.$content);
             transcoder.classToStyle(this.$content);
             transcoder.fontToImg(this.$content);
         }