marchon/odoo

[IMP] sale_timesheet: selling project based on template
Selling product creating project can now be based
on project template. When setting a project template
on product, this will copy the project (like a duplicate)
and replace its customer, ... with information from the
SO line, on SO confirmation.

There is only one project without template per SO, and
one project per template per SO (so 2 SO lines with the
same project template on their product will only create
one project with this template).

addons/sale_timesheet/tests/common.py
@@ -29,9 +29,19 @@ class TestCommonSaleTimesheetNoChart(TestCommonSaleNoChart):
             'user_type_id': cls.env.ref('account.data_account_type_revenue').id,
         })
 
+        # Create projects
         cls.project_global = cls.env['project.project'].create({
             'name': 'Project for selling timesheets',
             'allow_timesheets': True,
+        })
+        cls.project_template = cls.env['project.project'].create({
+            'name': 'Project TEMPLATE for services',
+            'allow_timesheets': True,
+        })
+        cls.project_template_state = cls.env['project.task.type'].create({
+            'name': 'Only stage in project template',
+            'sequence': 1,
+            'project_ids': [(4, cls.project_template.id)]
         })
 
         # Create service products
@@ -98,6 +108,22 @@ class TestCommonSaleTimesheetNoChart(TestCommonSaleNoChart):
             'taxes_id': False,
             'property_account_income_id': cls.account_sale.id,
         })
+        cls.product_order_timesheet5 = cls.env['product.product'].create({
+            'name': "Service Ordered, create project only based on template",
+            'standard_price': 17,
+            'list_price': 34,
+            'type': 'service',
+            'invoice_policy': 'order',
+            'uom_id': cls.env.ref('product.product_uom_hour').id,
+            'uom_po_id': cls.env.ref('product.product_uom_hour').id,
+            'default_code': 'SERV-ORDERED4',
+            'service_type': 'timesheet',
+            'service_tracking': 'project_only',
+            'project_id': False,
+            'project_template_id': cls.project_template.id,
+            'taxes_id': False,
+            'property_account_income_id': cls.account_sale.id,
+        })
 
         # -- timesheet on tasks (delivered, timesheet)
         cls.product_delivery_timesheet1 = cls.env['product.product'].create({
@@ -156,6 +182,22 @@ class TestCommonSaleTimesheetNoChart(TestCommonSaleNoChart):
             'default_code': 'SERV-DELI4',
             'service_type': 'timesheet',
             'service_tracking': 'project_only',
+            'project_id': False,
+            'taxes_id': False,
+            'property_account_income_id': cls.account_sale.id,
+        })
+        cls.product_delivery_timesheet5 = cls.env['product.product'].create({
+            'name': "Service delivered, create project only based on template",
+            'standard_price': 17,
+            'list_price': 34,
+            'type': 'service',
+            'invoice_policy': 'delivery',
+            'uom_id': cls.env.ref('product.product_uom_hour').id,
+            'uom_po_id': cls.env.ref('product.product_uom_hour').id,
+            'default_code': 'SERV-DELI4',
+            'service_type': 'timesheet',
+            'service_tracking': 'project_only',
+            'project_template_id': cls.project_template.id,
             'project_id': False,
             'taxes_id': False,
             'property_account_income_id': cls.account_sale.id,
@@ -219,6 +261,22 @@ class TestCommonSaleTimesheetNoChart(TestCommonSaleNoChart):
             'service_type': 'manual',
             'service_tracking': 'project_only',
             'project_id': False,
+            'taxes_id': False,
+            'property_account_income_id': cls.account_sale.id,
+        })
+        cls.product_delivery_manual5 = cls.env['product.product'].create({
+            'name': "Service delivered, create project only with template",
+            'standard_price': 17,
+            'list_price': 34,
+            'type': 'service',
+            'invoice_policy': 'delivery',
+            'uom_id': cls.env.ref('product.product_uom_hour').id,
+            'uom_po_id': cls.env.ref('product.product_uom_hour').id,
+            'default_code': 'SERV-DELI4',
+            'service_type': 'manual',
+            'service_tracking': 'project_only',
+            'project_id': False,
+            'project_template_id': cls.project_template.id,
             'taxes_id': False,
             'property_account_income_id': cls.account_sale.id,
         })

addons/sale_timesheet/tests/test_sale_service.py
@@ -126,7 +126,6 @@ class TestSaleService(TestCommonSaleTimesheetNoChart):
         self.sale_order.action_invoice_create()
         self.assertEqual(self.sale_order.invoice_status, 'invoiced', 'Sale Timesheet: "invoice on delivery" timesheets should not modify the invoice_status of the so')
 
-
     def test_task_so_line_assignation(self):
         # create SO line and confirm it
         so_line_deliver_global_project = self.env['sale.order.line'].create({
@@ -225,3 +224,112 @@ class TestSaleService(TestCommonSaleTimesheetNoChart):
         # remove timesheet2
         timesheet2.unlink()
         self.assertEqual(so_line_deliver_new_task_project.qty_delivered, timesheet3.unit_amount, 'Delivered quantity should be reset to the sum of remaining timesheets unit amounts.')
+
+    def test_sale_create_project(self):
+        """ A SO with multiple product that should create project (with and without template) like ;
+                Line 1 : Service 1 create project with Template A ===> project created with template A
+                Line 2 : Service 2 create project no template ==> empty project created
+                Line 3 : Service 3 create project with Template A ===> Don't create any project because line 1 has already created a project with template A
+                Line 4 : Service 4 create project no template ==> Don't create any project because line 2 has already created an empty project
+                Line 5 : Service 5 create project with Template B ===> project created with template B
+        """
+        # second project template and its associated product
+        project_template2 = self.env['project.project'].create({
+            'name': 'Second Project TEMPLATE for services',
+            'allow_timesheets': True,
+        })
+        Stage = self.env['project.task.type'].with_context(default_project_id=project_template2.id)
+        stage1_tmpl2 = Stage.create({
+            'name': 'Stage 1',
+            'sequence': 1
+        })
+        stage2_tmpl2 = Stage.create({
+            'name': 'Stage 2',
+            'sequence': 2
+        })
+        product_deli_ts_tmpl = self.env['product.product'].create({
+            'name': "Service delivered, create project only based on template B",
+            'standard_price': 17,
+            'list_price': 34,
+            'type': 'service',
+            'invoice_policy': 'delivery',
+            'uom_id': self.env.ref('product.product_uom_hour').id,
+            'uom_po_id': self.env.ref('product.product_uom_hour').id,
+            'default_code': 'SERV-DELI4',
+            'service_type': 'timesheet',
+            'service_tracking': 'project_only',
+            'project_template_id': project_template2.id,
+            'project_id': False,
+            'taxes_id': False,
+            'property_account_income_id': self.account_sale.id,
+        })
+
+        # create 5 so lines
+        so_line1 = self.env['sale.order.line'].create({
+            'name': self.product_delivery_timesheet5.name,
+            'product_id': self.product_delivery_timesheet5.id,
+            'product_uom_qty': 11,
+            'product_uom': self.product_delivery_timesheet5.uom_id.id,
+            'price_unit': self.product_delivery_timesheet5.list_price,
+            'order_id': self.sale_order.id,
+        })
+        so_line2 = self.env['sale.order.line'].create({
+            'name': self.product_order_timesheet4.name,
+            'product_id': self.product_order_timesheet4.id,
+            'product_uom_qty': 10,
+            'product_uom': self.product_order_timesheet4.uom_id.id,
+            'price_unit': self.product_order_timesheet4.list_price,
+            'order_id': self.sale_order.id,
+        })
+        so_line3 = self.env['sale.order.line'].create({
+            'name': self.product_delivery_timesheet5.name,
+            'product_id': self.product_delivery_timesheet5.id,
+            'product_uom_qty': 5,
+            'product_uom': self.product_delivery_timesheet5.uom_id.id,
+            'price_unit': self.product_delivery_timesheet5.list_price,
+            'order_id': self.sale_order.id,
+        })
+        so_line4 = self.env['sale.order.line'].create({
+            'name': self.product_delivery_manual3.name,
+            'product_id': self.product_delivery_manual3.id,
+            'product_uom_qty': 4,
+            'product_uom': self.product_delivery_manual3.uom_id.id,
+            'price_unit': self.product_delivery_manual3.list_price,
+            'order_id': self.sale_order.id,
+        })
+        so_line5 = self.env['sale.order.line'].create({
+            'name': product_deli_ts_tmpl.name,
+            'product_id': product_deli_ts_tmpl.id,
+            'product_uom_qty': 8,
+            'product_uom': product_deli_ts_tmpl.uom_id.id,
+            'price_unit': product_deli_ts_tmpl.list_price,
+            'order_id': self.sale_order.id,
+        })
+
+        # confirm SO
+        self.sale_order.action_confirm()
+
+        # check each line has or no generate something
+        self.assertTrue(so_line1.project_id, "Line1 should have create a project based on template A")
+        self.assertTrue(so_line2.project_id, "Line2 should have create an empty project")
+        self.assertFalse(so_line3.project_id, "Line3 should not have create a project, since line1 already create a project based on template A")
+        self.assertFalse(so_line4.project_id, "Line4 should not have create a project, since line1 already create an empty project")
+        self.assertTrue(so_line4.task_id, "Line4 should have create a new task, even if no project created.")
+        self.assertTrue(so_line5.project_id, "Line5 should have create a project based on template B")
+
+        # check generated stuff are correct
+        self.assertTrue(so_line1.project_id in self.project_template_state.project_ids, "Stage 1 from template B should be part of project from so line 1")
+        self.assertTrue(so_line1.project_id in self.project_template_state.project_ids, "Stage 1 from template B should be part of project from so line 1")
+
+        self.assertTrue(so_line5.project_id in stage1_tmpl2.project_ids, "Stage 1 from template B should be part of project from so line 5")
+        self.assertTrue(so_line5.project_id in stage2_tmpl2.project_ids, "Stage 2 from template B should be part of project from so line 5")
+
+        self.assertTrue(so_line1.project_id.allow_timesheets, "Create project should allow timesheets")
+        self.assertTrue(so_line2.project_id.allow_timesheets, "Create project should allow timesheets")
+        self.assertTrue(so_line5.project_id.allow_timesheets, "Create project should allow timesheets")
+
+        self.assertEqual(so_line4.task_id.project_id, so_line2.project_id, "Task created with line 4 should have the project based on template A of the SO.")
+
+        self.assertEqual(so_line1.project_id.sale_line_id, so_line1, "SO line of project with template A should be the one that create it.")
+        self.assertEqual(so_line2.project_id.sale_line_id, so_line2, "SO line of project should be the one that create it.")
+        self.assertEqual(so_line5.project_id.sale_line_id, so_line5, "SO line of project with template B should be the one that create it.")

cogitoweb/OCB

addons/sale_timesheet/tests/common.py
@@ -29,9 +29,19 @@ class TestCommonSaleTimesheetNoChart(TestCommonSaleNoChart):
             'user_type_id': cls.env.ref('account.data_account_type_revenue').id,
         })
 
+        # Create projects
         cls.project_global = cls.env['project.project'].create({
             'name': 'Project for selling timesheets',
             'allow_timesheets': True,
+        })
+        cls.project_template = cls.env['project.project'].create({
+            'name': 'Project TEMPLATE for services',
+            'allow_timesheets': True,
+        })
+        cls.project_template_state = cls.env['project.task.type'].create({
+            'name': 'Only stage in project template',
+            'sequence': 1,
+            'project_ids': [(4, cls.project_template.id)]
         })
 
         # Create service products
@@ -98,6 +108,22 @@ class TestCommonSaleTimesheetNoChart(TestCommonSaleNoChart):
             'taxes_id': False,
             'property_account_income_id': cls.account_sale.id,
         })
+        cls.product_order_timesheet5 = cls.env['product.product'].create({
+            'name': "Service Ordered, create project only based on template",
+            'standard_price': 17,
+            'list_price': 34,
+            'type': 'service',
+            'invoice_policy': 'order',
+            'uom_id': cls.env.ref('uom.product_uom_hour').id,
+            'uom_po_id': cls.env.ref('uom.product_uom_hour').id,
+            'default_code': 'SERV-ORDERED4',
+            'service_type': 'timesheet',
+            'service_tracking': 'project_only',
+            'project_id': False,
+            'project_template_id': cls.project_template.id,
+            'taxes_id': False,
+            'property_account_income_id': cls.account_sale.id,
+        })
 
         # -- timesheet on tasks (delivered, timesheet)
         cls.product_delivery_timesheet1 = cls.env['product.product'].create({
@@ -156,6 +182,22 @@ class TestCommonSaleTimesheetNoChart(TestCommonSaleNoChart):
             'default_code': 'SERV-DELI4',
             'service_type': 'timesheet',
             'service_tracking': 'project_only',
+            'project_id': False,
+            'taxes_id': False,
+            'property_account_income_id': cls.account_sale.id,
+        })
+        cls.product_delivery_timesheet5 = cls.env['product.product'].create({
+            'name': "Service delivered, create project only based on template",
+            'standard_price': 17,
+            'list_price': 34,
+            'type': 'service',
+            'invoice_policy': 'delivery',
+            'uom_id': cls.env.ref('uom.product_uom_hour').id,
+            'uom_po_id': cls.env.ref('uom.product_uom_hour').id,
+            'default_code': 'SERV-DELI4',
+            'service_type': 'timesheet',
+            'service_tracking': 'project_only',
+            'project_template_id': cls.project_template.id,
             'project_id': False,
             'taxes_id': False,
             'property_account_income_id': cls.account_sale.id,
@@ -219,6 +261,22 @@ class TestCommonSaleTimesheetNoChart(TestCommonSaleNoChart):
             'service_type': 'manual',
             'service_tracking': 'project_only',
             'project_id': False,
+            'taxes_id': False,
+            'property_account_income_id': cls.account_sale.id,
+        })
+        cls.product_delivery_manual5 = cls.env['product.product'].create({
+            'name': "Service delivered, create project only with template",
+            'standard_price': 17,
+            'list_price': 34,
+            'type': 'service',
+            'invoice_policy': 'delivery',
+            'uom_id': cls.env.ref('uom.product_uom_hour').id,
+            'uom_po_id': cls.env.ref('uom.product_uom_hour').id,
+            'default_code': 'SERV-DELI4',
+            'service_type': 'manual',
+            'service_tracking': 'project_only',
+            'project_id': False,
+            'project_template_id': cls.project_template.id,
             'taxes_id': False,
             'property_account_income_id': cls.account_sale.id,
         })

addons/sale_timesheet/tests/test_sale_service.py
@@ -125,7 +125,6 @@ class TestSaleService(TestCommonSaleTimesheetNoChart):
         self.sale_order.action_invoice_create()
         self.assertEqual(self.sale_order.invoice_status, 'invoiced', 'Sale Timesheet: "invoice on delivery" timesheets should not modify the invoice_status of the so')
 
-
     def test_task_so_line_assignation(self):
         # create SO line and confirm it
         so_line_deliver_global_project = self.env['sale.order.line'].create({
@@ -224,3 +223,112 @@ class TestSaleService(TestCommonSaleTimesheetNoChart):
         # remove timesheet2
         timesheet2.unlink()
         self.assertEqual(so_line_deliver_new_task_project.qty_delivered, timesheet3.unit_amount, 'Delivered quantity should be reset to the sum of remaining timesheets unit amounts.')
+
+    def test_sale_create_project(self):
+        """ A SO with multiple product that should create project (with and without template) like ;
+                Line 1 : Service 1 create project with Template A ===> project created with template A
+                Line 2 : Service 2 create project no template ==> empty project created
+                Line 3 : Service 3 create project with Template A ===> Don't create any project because line 1 has already created a project with template A
+                Line 4 : Service 4 create project no template ==> Don't create any project because line 2 has already created an empty project
+                Line 5 : Service 5 create project with Template B ===> project created with template B
+        """
+        # second project template and its associated product
+        project_template2 = self.env['project.project'].create({
+            'name': 'Second Project TEMPLATE for services',
+            'allow_timesheets': True,
+        })
+        Stage = self.env['project.task.type'].with_context(default_project_id=project_template2.id)
+        stage1_tmpl2 = Stage.create({
+            'name': 'Stage 1',
+            'sequence': 1
+        })
+        stage2_tmpl2 = Stage.create({
+            'name': 'Stage 2',
+            'sequence': 2
+        })
+        product_deli_ts_tmpl = self.env['product.product'].create({
+            'name': "Service delivered, create project only based on template B",
+            'standard_price': 17,
+            'list_price': 34,
+            'type': 'service',
+            'invoice_policy': 'delivery',
+            'uom_id': self.env.ref('uom.product_uom_hour').id,
+            'uom_po_id': self.env.ref('uom.product_uom_hour').id,
+            'default_code': 'SERV-DELI4',
+            'service_type': 'timesheet',
+            'service_tracking': 'project_only',
+            'project_template_id': project_template2.id,
+            'project_id': False,
+            'taxes_id': False,
+            'property_account_income_id': self.account_sale.id,
+        })
+
+        # create 5 so lines
+        so_line1 = self.env['sale.order.line'].create({
+            'name': self.product_delivery_timesheet5.name,
+            'product_id': self.product_delivery_timesheet5.id,
+            'product_uom_qty': 11,
+            'product_uom': self.product_delivery_timesheet5.uom_id.id,
+            'price_unit': self.product_delivery_timesheet5.list_price,
+            'order_id': self.sale_order.id,
+        })
+        so_line2 = self.env['sale.order.line'].create({
+            'name': self.product_order_timesheet4.name,
+            'product_id': self.product_order_timesheet4.id,
+            'product_uom_qty': 10,
+            'product_uom': self.product_order_timesheet4.uom_id.id,
+            'price_unit': self.product_order_timesheet4.list_price,
+            'order_id': self.sale_order.id,
+        })
+        so_line3 = self.env['sale.order.line'].create({
+            'name': self.product_delivery_timesheet5.name,
+            'product_id': self.product_delivery_timesheet5.id,
+            'product_uom_qty': 5,
+            'product_uom': self.product_delivery_timesheet5.uom_id.id,
+            'price_unit': self.product_delivery_timesheet5.list_price,
+            'order_id': self.sale_order.id,
+        })
+        so_line4 = self.env['sale.order.line'].create({
+            'name': self.product_delivery_manual3.name,
+            'product_id': self.product_delivery_manual3.id,
+            'product_uom_qty': 4,
+            'product_uom': self.product_delivery_manual3.uom_id.id,
+            'price_unit': self.product_delivery_manual3.list_price,
+            'order_id': self.sale_order.id,
+        })
+        so_line5 = self.env['sale.order.line'].create({
+            'name': product_deli_ts_tmpl.name,
+            'product_id': product_deli_ts_tmpl.id,
+            'product_uom_qty': 8,
+            'product_uom': product_deli_ts_tmpl.uom_id.id,
+            'price_unit': product_deli_ts_tmpl.list_price,
+            'order_id': self.sale_order.id,
+        })
+
+        # confirm SO
+        self.sale_order.action_confirm()
+
+        # check each line has or no generate something
+        self.assertTrue(so_line1.project_id, "Line1 should have create a project based on template A")
+        self.assertTrue(so_line2.project_id, "Line2 should have create an empty project")
+        self.assertFalse(so_line3.project_id, "Line3 should not have create a project, since line1 already create a project based on template A")
+        self.assertFalse(so_line4.project_id, "Line4 should not have create a project, since line1 already create an empty project")
+        self.assertTrue(so_line4.task_id, "Line4 should have create a new task, even if no project created.")
+        self.assertTrue(so_line5.project_id, "Line5 should have create a project based on template B")
+
+        # check generated stuff are correct
+        self.assertTrue(so_line1.project_id in self.project_template_state.project_ids, "Stage 1 from template B should be part of project from so line 1")
+        self.assertTrue(so_line1.project_id in self.project_template_state.project_ids, "Stage 1 from template B should be part of project from so line 1")
+
+        self.assertTrue(so_line5.project_id in stage1_tmpl2.project_ids, "Stage 1 from template B should be part of project from so line 5")
+        self.assertTrue(so_line5.project_id in stage2_tmpl2.project_ids, "Stage 2 from template B should be part of project from so line 5")
+
+        self.assertTrue(so_line1.project_id.allow_timesheets, "Create project should allow timesheets")
+        self.assertTrue(so_line2.project_id.allow_timesheets, "Create project should allow timesheets")
+        self.assertTrue(so_line5.project_id.allow_timesheets, "Create project should allow timesheets")
+
+        self.assertEqual(so_line4.task_id.project_id, so_line2.project_id, "Task created with line 4 should have the project based on template A of the SO.")
+
+        self.assertEqual(so_line1.project_id.sale_line_id, so_line1, "SO line of project with template A should be the one that create it.")
+        self.assertEqual(so_line2.project_id.sale_line_id, so_line2, "SO line of project should be the one that create it.")
+        self.assertEqual(so_line5.project_id.sale_line_id, so_line5, "SO line of project with template B should be the one that create it.")