marchon/odoo

[IMP] sale_timesheet: revamp project overview
The project overview is kind of badly design. This
commit does not solve all the issue but tries to make
it simpler and more efficient:
- Does not depends on AAL anymore: the overview search
is now about projects. You can see the overview of multiple
projects. Even when no timesheet are encoded, the plan will
not be empty.
- "Time by people" displays each employee assigned to task,
even if no timesheet are recorded.
- Code now use reporting view to keep consistency and simple
code in overview controller.

addons/sale_timesheet/models/project.py
@@ -41,16 +41,16 @@ class Project(models.Model):
 
     @api.multi
     def action_view_timesheet_plan(self):
-        return {
-            'name': _('Overview'),
-            'type': 'ir.actions.client',
-            'tag': 'timesheet.plan',
-            'context': {
-                'active_id': self.id,
-                'active_ids': self.ids,
-                'search_default_project_id': self.id,
-            }
+        action = self.env.ref('sale_timesheet.project_timesheet_action_client_timesheet_plan').read()[0]
+        action['params'] = {
+            'project_ids': self.ids,
         }
+        action['context'] = {
+            'active_id': self.id,
+            'active_ids': self.ids,
+            'search_default_name': self.name,
+        }
+        return action
 
 
 class ProjectTask(models.Model):

cogitoweb/OCB

addons/sale_timesheet/models/project.py
@@ -41,16 +41,16 @@ class Project(models.Model):
 
     @api.multi
     def action_view_timesheet_plan(self):
-        return {
-            'name': _('Overview'),
-            'type': 'ir.actions.client',
-            'tag': 'timesheet.plan',
-            'context': {
-                'active_id': self.id,
-                'active_ids': self.ids,
-                'search_default_project_id': self.id,
-            }
+        action = self.env.ref('sale_timesheet.project_timesheet_action_client_timesheet_plan').read()[0]
+        action['params'] = {
+            'project_ids': self.ids,
         }
+        action['context'] = {
+            'active_id': self.id,
+            'active_ids': self.ids,
+            'search_default_display_name': self.name,
+        }
+        return action
 
 
 class ProjectTask(models.Model):