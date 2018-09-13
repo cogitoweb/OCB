jleal-pnet/odoo

[IMP] mail: new attachment button in the chatter
- Added a new button to the chatter that displays the amount of
 attachments on the current record and when clicked, all the attachments
 attached to the current record

      - added mail.FormView (includes), used by the chatter attachment
       button to fetch the field value of attachment_count.

      - added a computed field "attachment_count" to mail.thread that
       contains the amount of attachment attached to the record.

      - changed the followers div, it is now sharing a "right side"
       area with the attachment button.

 task: 1860031

addons/mail/static/src/js/chatter.js
@@ -2,6 +2,7 @@ odoo.define('mail.Chatter', function (require) {
 "use strict";
 
 var Activity = require('mail.Activity');
+var AttachmentBox = require('mail.AttachmentBox');
 var ChatterComposer = require('mail.composer.Chatter');
 var Followers = require('mail.Followers');
 var ThreadField = require('mail.ThreadField');
@@ -30,6 +31,7 @@ var Chatter = Widget.extend({
     events: {
         'click .o_chatter_button_new_message': '_onOpenComposerMessage',
         'click .o_chatter_button_log_note': '_onOpenComposerNote',
+        'click .o_chatter_button_attachment': '_onOpenAttachments',
         'click .o_chatter_button_schedule_activity': '_onScheduleActivity',
     },
     supportedFieldTypes: ['one2many'],
@@ -69,21 +71,23 @@ var Chatter = Widget.extend({
             this.hasLogButton = options.display_log_button || nodeOptions.display_log_button;
             this.postRefresh = nodeOptions.post_refresh || 'never';
         }
+        this.attachmentBoxOpened = false;
     },
     /**
      * @override
      */
     start: function () {
         this._$topbar = this.$('.o_chatter_topbar');
-
+        this.$('.o_topbar_right_area').append(QWeb.render('mail.chatter.Attachment.Button', {
+            count: this.record.data.attachment_count || 0,
+        }));
         // render and append the buttons
-        this._$topbar.append(QWeb.render('mail.chatter.Buttons', {
+        this._$topbar.prepend(QWeb.render('mail.chatter.Buttons', {
             newMessageButton: !!this.fields.thread,
             logNoteButton: this.hasLogButton,
             scheduleActivityButton: !!this.fields.activity,
             isMobile: config.device.isMobile,
         }));
-
         // start and append the widgets
         var fieldDefs = _.invoke(this.fields, 'appendTo', $('<div>'));
         var def = this._dp.add($.when.apply($, fieldDefs));
@@ -107,6 +111,7 @@ var Chatter = Widget.extend({
         // close the composer if we switch to another record as it is record dependent
         if (this.record.res_id !== record.res_id) {
             this._closeComposer(true);
+            this._closeAttachments();
         }
 
         // update the state
@@ -138,12 +143,24 @@ var Chatter = Widget.extend({
             self.$el.height('auto');
             self._updateMentionSuggestions();
         });
+        this._updateAttachmentCounter();
     },
 
     //--------------------------------------------------------------------------
     // Private
     //--------------------------------------------------------------------------
 
+    /**
+     * @private
+     * @param {boolean} force
+     */
+    _closeAttachments: function () {
+        if (this.fields.attachments) {
+            this.$('.o_chatter_button_attachment').removeClass('o_active_attach');
+            this.fields.attachments.destroy();
+            this.attachmentBoxOpened = false;
+        }
+    },
     /**
      * @private
      * @param {boolean} force
@@ -201,6 +218,23 @@ var Chatter = Widget.extend({
     },
     /**
      * @private
+     */
+    _openAttachments: function () {
+        var self = this;
+        this.fields.attachments = new AttachmentBox(this, this.record);
+
+        var $anchor = this.$('.o_chatter_topbar');
+        if (this._composer) {
+            $anchor = this.$('.o_thread_composer');
+        }
+        this.fields.attachments.insertAfter($anchor).then(function () {
+            self.$el.addClass('o_chatter_composer_active');
+            self.$('.o_chatter_button_attachment').addClass('o_active_attach');
+        });
+        this.attachmentBoxOpened = true;
+    },
+    /**
+     * @private
      * @param {Object} options
      * @param {Object[]} [options.suggested_partners=[]]
      * @param {boolean} [options.isLog]
@@ -235,6 +269,8 @@ var Chatter = Widget.extend({
                         self._closeComposer(true);
                         if (self._reloadAfterPost(messageData)) {
                             self.trigger_up('reload');
+                        } else if (messageData.attachment_ids.length) {
+                            self.trigger_up('reload', {fieldNames: ['attachment_count']});
                         }
                     });
                 });
@@ -290,7 +326,7 @@ var Chatter = Widget.extend({
                 self.fields.activity.$el.appendTo(self.$el);
             }
             if (self.fields.followers) {
-                self.fields.followers.$el.appendTo(self._$topbar);
+                self.fields.followers.$el.insertBefore(self.$('.o_chatter_button_attachment'));
             }
             if (self.fields.thread) {
                 self.fields.thread.$el.appendTo(self.$el);
@@ -324,6 +360,14 @@ var Chatter = Widget.extend({
         this.record = record;
         this.recordName = record.data.display_name;
     },
+    /**
+     * @private
+     */
+     _updateAttachmentCounter: function () {
+        var count = this.record.data.attachment_count || 0;
+        this.$('.o_chatter_attachment_button_count').html(' ('+ count +')');
+        this.$('.o_chatter_button_attachment').toggleClass('o_hidden', !count);
+     },
     /**
      * @private
      */
@@ -363,6 +407,16 @@ var Chatter = Widget.extend({
     // Handlers
     //--------------------------------------------------------------------------
 
+    /**
+     * @private
+     */
+    _onOpenAttachments: function () {
+        if (this.attachmentBoxOpened) {
+            this._closeAttachments();
+        } else {
+            this._openAttachments();
+        }
+    },
     /**
      * Discard changes on the record.
      * This is notified by the composer, when opening the full-composer.

addons/mail/static/src/js/form_renderer.js
@@ -29,7 +29,8 @@ FormRenderer.include({
      */
     confirmChange: function (state, id, fields) {
         if (this.chatter) {
-            var updatedMailFields = _.intersection(fields, _.values(this.mailFields));
+            var chatterFields = ['attachment_count'].concat(_.values(this.mailFields));
+            var updatedMailFields = _.intersection(fields, chatterFields);
             if (updatedMailFields.length) {
                 this.chatter.update(state, updatedMailFields);
             }

addons/mail/static/src/js/form_view.js
@@ -1,0 +1,25 @@
+odoo.define('mail.FormView', function (require) {
+"use strict";
+
+var FormView = require('web.FormView');
+
+/**
+ * This file is used to add "attachment_count" to fieldsInfo so we can fetch its value for the
+ * chatter's attachment button without having it explicitly declared in the form view template.
+ *
+ */
+
+FormView.include({
+    /**
+     * @override
+     */
+    init: function () {
+        this._super.apply(this, arguments);
+
+        if ('message_ids' in this.fieldsInfo[this.viewType]) {
+            this.fieldsInfo[this.viewType].attachment_count = {};
+        }
+    },
+});
+
+});

cogitoweb/OCB

addons/mail/static/src/js/chatter.js
@@ -2,6 +2,7 @@ odoo.define('mail.Chatter', function (require) {
 "use strict";
 
 var Activity = require('mail.Activity');
+var AttachmentBox = require('mail.AttachmentBox');
 var ChatterComposer = require('mail.composer.Chatter');
 var Followers = require('mail.Followers');
 var ThreadField = require('mail.ThreadField');
@@ -30,6 +31,7 @@ var Chatter = Widget.extend({
     events: {
         'click .o_chatter_button_new_message': '_onOpenComposerMessage',
         'click .o_chatter_button_log_note': '_onOpenComposerNote',
+        'click .o_chatter_button_attachment': '_onOpenAttachments',
         'click .o_chatter_button_schedule_activity': '_onScheduleActivity',
     },
     supportedFieldTypes: ['one2many'],
@@ -69,21 +71,23 @@ var Chatter = Widget.extend({
             this.hasLogButton = options.display_log_button || nodeOptions.display_log_button;
             this.postRefresh = nodeOptions.post_refresh || 'never';
         }
+        this.attachmentBoxOpened = false;
     },
     /**
      * @override
      */
     start: function () {
         this._$topbar = this.$('.o_chatter_topbar');
-
+        this.$('.o_topbar_right_area').append(QWeb.render('mail.chatter.Attachment.Button', {
+            count: this.record.data.related_attachment_count || 0,
+        }));
         // render and append the buttons
-        this._$topbar.append(QWeb.render('mail.chatter.Buttons', {
+        this._$topbar.prepend(QWeb.render('mail.chatter.Buttons', {
             newMessageButton: !!this.fields.thread,
             logNoteButton: this.hasLogButton,
             scheduleActivityButton: !!this.fields.activity,
             isMobile: config.device.isMobile,
         }));
-
         // start and append the widgets
         var fieldDefs = _.invoke(this.fields, 'appendTo', $('<div>'));
         var def = this._dp.add($.when.apply($, fieldDefs));
@@ -107,6 +111,7 @@ var Chatter = Widget.extend({
         // close the composer if we switch to another record as it is record dependent
         if (this.record.res_id !== record.res_id) {
             this._closeComposer(true);
+            this._closeAttachments();
         }
 
         // update the state
@@ -138,12 +143,24 @@ var Chatter = Widget.extend({
             self.$el.height('auto');
             self._updateMentionSuggestions();
         });
+        this._updateAttachmentCounter();
     },
 
     //--------------------------------------------------------------------------
     // Private
     //--------------------------------------------------------------------------
 
+    /**
+     * @private
+     * @param {boolean} force
+     */
+    _closeAttachments: function () {
+        if (this.fields.attachments) {
+            this.$('.o_chatter_button_attachment').removeClass('o_active_attach');
+            this.fields.attachments.destroy();
+            this.attachmentBoxOpened = false;
+        }
+    },
     /**
      * @private
      * @param {boolean} force
@@ -201,6 +218,23 @@ var Chatter = Widget.extend({
     },
     /**
      * @private
+     */
+    _openAttachments: function () {
+        var self = this;
+        this.fields.attachments = new AttachmentBox(this, this.record);
+
+        var $anchor = this.$('.o_chatter_topbar');
+        if (this._composer) {
+            $anchor = this.$('.o_thread_composer');
+        }
+        this.fields.attachments.insertAfter($anchor).then(function () {
+            self.$el.addClass('o_chatter_composer_active');
+            self.$('.o_chatter_button_attachment').addClass('o_active_attach');
+        });
+        this.attachmentBoxOpened = true;
+    },
+    /**
+     * @private
      * @param {Object} options
      * @param {Object[]} [options.suggested_partners=[]]
      * @param {boolean} [options.isLog]
@@ -235,6 +269,8 @@ var Chatter = Widget.extend({
                         self._closeComposer(true);
                         if (self._reloadAfterPost(messageData)) {
                             self.trigger_up('reload');
+                        } else if (messageData.attachment_ids.length) {
+                            self.trigger_up('reload', {fieldNames: ['related_attachment_count']});
                         }
                     });
                 });
@@ -290,7 +326,7 @@ var Chatter = Widget.extend({
                 self.fields.activity.$el.appendTo(self.$el);
             }
             if (self.fields.followers) {
-                self.fields.followers.$el.appendTo(self._$topbar);
+                self.fields.followers.$el.insertBefore(self.$('.o_chatter_button_attachment'));
             }
             if (self.fields.thread) {
                 self.fields.thread.$el.appendTo(self.$el);
@@ -324,6 +360,14 @@ var Chatter = Widget.extend({
         this.record = record;
         this.recordName = record.data.display_name;
     },
+    /**
+     * @private
+     */
+     _updateAttachmentCounter: function () {
+        var count = this.record.data.related_attachment_count || 0;
+        this.$('.o_chatter_attachment_button_count').html(' ('+ count +')');
+        this.$('.o_chatter_button_attachment').toggleClass('o_hidden', !count);
+     },
     /**
      * @private
      */
@@ -363,6 +407,16 @@ var Chatter = Widget.extend({
     // Handlers
     //--------------------------------------------------------------------------
 
+    /**
+     * @private
+     */
+    _onOpenAttachments: function () {
+        if (this.attachmentBoxOpened) {
+            this._closeAttachments();
+        } else {
+            this._openAttachments();
+        }
+    },
     /**
      * Discard changes on the record.
      * This is notified by the composer, when opening the full-composer.

addons/mail/static/src/js/form_renderer.js
@@ -29,7 +29,8 @@ FormRenderer.include({
      */
     confirmChange: function (state, id, fields) {
         if (this.chatter) {
-            var updatedMailFields = _.intersection(fields, _.values(this.mailFields));
+            var chatterFields = ['related_attachment_count'].concat(_.values(this.mailFields));
+            var updatedMailFields = _.intersection(fields, chatterFields);
             if (updatedMailFields.length) {
                 this.chatter.update(state, updatedMailFields);
             }

addons/mail/static/src/js/form_view.js
@@ -1,0 +1,25 @@
+odoo.define('mail.FormView', function (require) {
+"use strict";
+
+var FormView = require('web.FormView');
+
+/**
+ * This file is used to add "related_attachment_count" to fieldsInfo so we can fetch its value for the
+ * chatter's attachment button without having it explicitly declared in the form view template.
+ *
+ */
+
+FormView.include({
+    /**
+     * @override
+     */
+    init: function () {
+        this._super.apply(this, arguments);
+
+        if ('message_ids' in this.fieldsInfo[this.viewType]) {
+            this.fieldsInfo[this.viewType].related_attachment_count = {};
+        }
+    },
+});
+
+});