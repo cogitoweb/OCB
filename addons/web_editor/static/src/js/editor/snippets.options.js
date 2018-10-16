cogitoweb/OCB

commit 451d814e0ba0f942ebf3bce665409e4a41816268

[IMP] website: more intuitive image and link edition
- Remove useless snippet background suggestions (only one button to
  open the media dialog directly)
- Show meaningful-only tabs in the media dialog (no document as
  background, ...)
- When editing a background shows the background suggestions first and
  show the background suggestions last for standard images. Technically,
  this is done by filtering on the name/fname of the attachments and
  adding magic name parts to the fname on image upload (e.g. when
  uploading an image from the media dialog when customizing a bg, the
  uploaded image will automatically be marked as a background). Note:
  there were discussions to implement this feature with a tag system for
  ir.attachment/s but the implementation's cost was too great in regards
  to the feature.
- When editing an existing link or creating one from selected text, do
  not show the "Link Label" field in the dialog; it was confusing. Only
  show that field when adding a link at the cursor location with no
  selected text.

addons/web_editor/static/src/js/editor/snippets.options.js
@@ -715,7 +715,7 @@ registry.colorpicker = SnippetOption.extend({
         this.$el.find('.colorpicker button.selected').removeClass('selected');
         $(ev.currentTarget).addClass('selected');
         this.$target.closest('.o_editable').trigger('content_changed');
-        this.$target.trigger('background-color-event', ev.type);
+        this.$target.trigger('background-color-event', false);
     },
     /**
      * Called when a color button is entered -> preview the background color.
@@ -729,7 +729,7 @@ registry.colorpicker = SnippetOption.extend({
         if (color) {
             this.$target.addClass(this.colorPrefix + color);
         }
-        this.$target.trigger('background-color-event', ev.type);
+        this.$target.trigger('background-color-event', true);
     },
     /**
      * Called when a color button is left -> cancel the preview.
@@ -744,7 +744,7 @@ registry.colorpicker = SnippetOption.extend({
         if (color) {
             this.$target.addClass(this.colorPrefix + color);
         }
-        this.$target.trigger('background-color-event', ev.type);
+        this.$target.trigger('background-color-event', 'reset');
     },
     /**
      * Called when the color reset button is clicked -> remove all background
@@ -813,10 +813,10 @@ registry.background = SnippetOption.extend({
         // Put fake image in the DOM, edit it and use it as background-image
         var $image = $('<img/>', {class: 'hidden', src: value}).appendTo(this.$target);
 
-        var _editor = new weWidgets.MediaDialog(this, {}, null, $image[0]).open();
-        _editor.opened(function () {
-            _editor.$('[href="#editor-media-video"], [href="#editor-media-icon"]').addClass('hidden');
-        });
+        var _editor = new weWidgets.MediaDialog(this, {
+            onlyImages: true,
+            firstFilters: ['background'],
+        }, null, $image[0]).open();
 
         _editor.on('save', this, function () {
             this._setCustomBackground($image.attr('src'));
@@ -836,10 +836,13 @@ registry.background = SnippetOption.extend({
      */
     bindBackgroundEvents: function () {
         this.$target.off('.background-option')
-            .on('background-color-event.background-option', (function (e, type) {
+            .on('background-color-event.background-option', (function (e, previewMode) {
                 e.stopPropagation();
                 if (e.currentTarget !== e.target) return;
-                this.$el.find('li[data-background=""] > a').trigger(type);
+                if (previewMode === false) {
+                    this.__customImageSrc = undefined;
+                }
+                this.background(previewMode);
             }).bind(this));
     },
     /**

addons/web_editor/controllers/main.py
@@ -140,7 +140,7 @@ class Web_Editor(http.Controller):
     # add attachment (images or link)
     #------------------------------------------------------
     @http.route('/web_editor/attachment/add', type='http', auth='user', methods=['POST'])
-    def attach(self, upload=None, url=None, disable_optimization=None, **kwargs):
+    def attach(self, upload=None, url=None, disable_optimization=None, filters=None, **kwargs):
         # the upload argument doesn't allow us to access the files if more than
         # one file is uploaded, as upload references the first file
         # therefore we have to recover the files from the request object
@@ -150,8 +150,12 @@ class Web_Editor(http.Controller):
         message = None
         if not upload: # no image provided, storing the link and the image name
             name = url.split("/").pop()                       # recover filename
+            datas_fname = name
+            if filters:
+                datas_fname = filters + '_' + datas_fname
             attachment = Attachments.create({
                 'name': name,
+                'datas_fname': datas_fname,
                 'type': 'url',
                 'url': url,
                 'public': True,
@@ -175,10 +179,14 @@ class Web_Editor(http.Controller):
                     except IOError as e:
                         pass
 
+                    name = c_file.filename
+                    datas_fname = name
+                    if filters:
+                        datas_fname = filters + '_' + datas_fname
                     attachment = Attachments.create({
-                        'name': c_file.filename,
+                        'name': name,
                         'datas': base64.b64encode(data),
-                        'datas_fname': c_file.filename,
+                        'datas_fname': datas_fname,
                         'public': True,
                         'res_model': 'ir.ui.view',
                     })

addons/web_editor/static/src/js/editor/rte.summernote.js
@@ -395,6 +395,9 @@ eventHandler.modules.imageDialog.showImageDialog = function ($editable) {
     core.bus.trigger('media_dialog_demand', {
         $editable: $editable,
         media: media,
+        options: {
+            lastFilters: ['background'],
+        },
     });
     return new $.Deferred().reject();
 };

addons/web_editor/static/src/js/tours/rte.js
@@ -171,22 +171,17 @@ tour.register('rte', {
     trigger: '.note-image-popover:visible button[data-event="showLinkDialog"]',
     extra_trigger: '#editable_area > section .row > div:first span.fa.pull-right',
 }, {
+    content: "insert a link url",
+    trigger: 'input[name="url"]',
+    run: "text http://www.odoo.com",
+}, {
     content: "click on color style",
     trigger: '.o_link_dialog_color > .o_link_dialog_color_item.btn-success',
     extra_trigger: 'a#link-preview:containsRegex(/^<span [^>]+><\\/span>$/) > span.fa.fa-3x.pull-right',
 }, {
-    content: "insert a link url",
-    trigger: 'input[name="url"]',
-    extra_trigger: 'a#link-preview.btn',
-    run: "text http://www.odoo.com",
-}, {
-    content: "change text label",
-    trigger: '#o_link_dialog_label_input',
-    run: "text ABC[IMG] DEF",
-}, {
     content: "save link",
     trigger: '.modal-footer > .btn-primary',
-    extra_trigger: 'a#link-preview.btn:containsRegex(/^ABC<span [^>]+><\\/span> DEF$/)',
+    extra_trigger: 'a#link-preview.btn.btn-success:containsRegex(/^<span [^>]+><\\/span>$/) > span.fa.fa-3x.pull-right',
 }, {
     content: "click on other picture",
     trigger: '#editable_area > section .row > div:last img',
@@ -196,18 +191,17 @@ tour.register('rte', {
     trigger: '.note-image-popover:visible button[data-event="showLinkDialog"]',
     extra_trigger: '#editable_area > section .row > div:first span.fa.pull-right',
 }, {
+    content: "insert an email",
+    trigger: 'input[name="url"]',
+    run: "text test@test.test",
+}, {
     content: "click on color style again",
     trigger: '.o_link_dialog_color > .o_link_dialog_color_item.btn-success',
     extra_trigger: 'a#link-preview:containsRegex(/^<img [^>]+>$/) img',
 }, {
-    content: "insert an email",
-    trigger: 'input[name="url"]',
-    extra_trigger: 'a#link-preview.btn',
-    run: "text test@test.test",
-}, {
     content: "save link",
     trigger: '.modal-footer > .btn-primary',
-    extra_trigger: 'a#link-preview.btn[href="mailto:test@test.test"]',
+    extra_trigger: 'a#link-preview.btn.btn-success[href="mailto:test@test.test"]:containsRegex(/^<img [^>]+>$/) img',
 }, {
     content: "select for triple enter then double backspace",
     trigger: '#editable_area > section .row > div:first p:eq(2)',

addons/web_editor/static/src/js/widgets/widgets.js
@@ -215,6 +215,9 @@ var ImageWidget = MediaWidget.extend({
             ];
         this.multiImages = options.multiImages;
 
+        this.firstFilters = options.firstFilters || [];
+        this.lastFilters = options.lastFilters || [];
+
         this.images = [];
     },
     /**
@@ -321,16 +324,35 @@ var ImageWidget = MediaWidget.extend({
             args: [],
             kwargs: {
                 domain: domain,
-                fields: ['name', 'mimetype', 'checksum', 'url', 'type'],
+                fields: ['name', 'datas_fname', 'mimetype', 'checksum', 'url', 'type'],
                 order: [{name: 'id', asc: false}],
                 context: weContext.get(),
             },
         }).then(function (records) {
-            self.records = _.uniq(_.filter(records, function (r) {
-                return (r.type === "binary" || r.url && r.url.length > 0);
-            }), function (r) {
-                return (r.url || r.id);
-            });
+            self.records = _.chain(records)
+                .filter(function (r) {
+                    return (r.type === "binary" || r.url && r.url.length > 0);
+                })
+                .uniq(function (r) {
+                    return (r.url || r.id);
+                })
+                .sortBy(function (r) {
+                    if (_.any(self.firstFilters, function (filter) {
+                        var regex = new RegExp(filter, 'i');
+                        return r.name.match(regex) || r.datas_fname.match(regex);
+                    })) {
+                        return -1;
+                    }
+                    if (_.any(self.lastFilters, function (filter) {
+                        var regex = new RegExp(filter, 'i');
+                        return r.name.match(regex) || r.datas_fname.match(regex);
+                    })) {
+                        return 1;
+                    }
+                    return 0;
+                })
+                .value();
+
             _.each(self.records, function (record) {
                 record.src = record.url || _.str.sprintf('/web/image/%s/%s', record.id, record.name); // Name is added for SEO purposes
                 record.isDocument = !(/gif|jpe|jpg|png/.test(record.mimetype));
@@ -1045,12 +1067,22 @@ var MediaDialog = Dialog.extend({
         this.range = range.create();
 
         this.multiImages = options.multiImages;
-        this.onlyImages = options.onlyImages || this.multiImages || (this.media && (this.$media.parent().data('oeField') === 'image' || this.$media.parent().data('oeType') === 'image'));
+        var onlyImages = options.onlyImages || this.multiImages || (this.media && (this.$media.parent().data('oeField') === 'image' || this.$media.parent().data('oeType') === 'image'));
+        this.noImages = options.noImages;
+        this.noDocuments = onlyImages || options.noDocuments;
+        this.noIcons = onlyImages || options.noIcons;
+        this.noVideos = onlyImages || options.noVideos;
 
-        this.imageDialog = new ImageWidget(this, this.media, options);
-        this.documentDialog = new ImageWidget(this, this.media, _.extend({}, options, {document: true}));
-        if (!this.onlyImages) {
+        if (!this.noImages) {
+            this.imageDialog = new ImageWidget(this, this.media, options);
+        }
+        if (!this.noDocuments) {
+            this.documentDialog = new ImageWidget(this, this.media, _.extend({}, options, {document: true}));
+        }
+        if (!this.noIcons) {
             this.iconDialog = new IconWidget(this, this.media, options);
+        }
+        if (!this.noVideos) {
             this.videoDialog = new VideoWidget(this, this.media, options);
         }
 
@@ -1245,16 +1277,20 @@ var LinkDialog = Dialog.extend({
         this._super(parent, _.extend({
             title: _t("Link to"),
         }, options || {}));
+
         this.editable = editable;
         this.data = linkInfo || {};
 
         this.data.className = "";
+
+        var r = this.data.range;
+        this.needLabel = !r || (r.sc === r.ec && r.so === r.eo);
+
         if (this.data.range) {
             this.data.iniClassName = $(this.data.range.sc).filter("a").attr("class") || "";
             this.data.className = this.data.iniClassName.replace(/(^|\s+)btn(-[a-z0-9_-]*)?/gi, ' ');
 
             var is_link = this.data.range.isOnAnchor();
-            var r = this.data.range;
 
             var sc = r.sc;
             var so = r.so;

addons/web_editor/static/src/xml/editor.xml
@@ -47,10 +47,10 @@
     <!-- Media Dialog (allows to choose an img/pictogram/video) -->
     <div t-name="web_editor.dialog.media">
         <ul class="nav nav-tabs">
-            <li class="active"><a href="#editor-media-image" data-toggle="tab">Image</a></li>
-            <li><a href="#editor-media-document" data-toggle="tab" t-att-class="widget.onlyImages ? 'hidden' : ''">Document</a></li>
-            <li><a href="#editor-media-icon" data-toggle="tab" t-att-class="widget.onlyImages ? 'hidden' : ''">Pictogram</a></li>
-            <li><a href="#editor-media-video" data-toggle="tab" t-att-class="widget.onlyImages ? 'hidden' : ''">Video</a></li>
+            <li class="active"><a href="#editor-media-image" data-toggle="tab" t-att-class="widget.noImages ? 'hidden' : ''">Image</a></li>
+            <li><a href="#editor-media-document" data-toggle="tab" t-att-class="widget.noDocuments ? 'hidden' : ''">Document</a></li>
+            <li><a href="#editor-media-icon" data-toggle="tab" t-att-class="widget.noIcons ? 'hidden' : ''">Pictogram</a></li>
+            <li><a href="#editor-media-video" data-toggle="tab" t-att-class="widget.noVideos ? 'hidden' : ''">Video</a></li>
             <li class="search pull-right">
                 <ul class="pager mb0 mt0">
                     <li class="previous">
@@ -85,6 +85,7 @@
             enctype="multipart/form-data"
             target="fileframe">
             <input type="hidden" name="csrf_token" t-att-value="csrf_token"/>
+            <input type="hidden" name="filters" t-att-value="widget.firstFilters.join('_')"/>
             <div class="well">
                 <div class="form-group pull-left">
                     <input type="file" class="hidden" name="upload" t-att-accept="widget.accept" multiple="multiple"/>
@@ -234,7 +235,7 @@
     <div t-name="web_editor.dialog.link" class="o_link_dialog">
         <div class="row">
             <form class="col-md-8 form-horizontal">
-                <div class="form-group">
+                <div t-attf-class="form-group#{widget.needLabel ? '' : ' hidden'}">
                     <label class="control-label col-sm-3" for="o_link_dialog_label_input">Link Label</label>
                     <div class="col-sm-9">
                         <input type="text" name="label" class="form-control" id="o_link_dialog_label_input" required="required" t-att-value="widget.data.text"/>

addons/website/data/website_data.xml
@@ -142,7 +142,6 @@
                 </t>
             </field>
         </record>
-
         <record id="aboutus_page" model="website.page">
             <field name="website_published">True</field>
             <field name="url">/aboutus</field>
@@ -208,7 +207,6 @@
             <field name="type">binary</field>
             <field name="datas" type="base64" file="website/static/src/img/library/business_conference.jpg"/>
         </record>
-
         <record id="website.library_image_01" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Website Library Image 01</field>
@@ -217,7 +215,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/library/bridge.jpg</field>
         </record>
-
         <record id="website.library_image_02" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Website Library Image 02</field>
@@ -226,7 +223,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/library/deliver.jpg</field>
         </record>
-
         <record id="website.library_image_03" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Website Library Image 03</field>
@@ -235,7 +231,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/library/clock.jpg</field>
         </record>
-
         <record id="website.library_image_04" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Website Library Image 04</field>
@@ -244,7 +239,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/library/manufacturing.jpg</field>
         </record>
-
         <record id="website.library_image_05" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Website Library Image 05</field>
@@ -253,7 +247,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/library/compass.jpg</field>
         </record>
-
         <record id="website.library_image_06" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Website Library Image 06</field>
@@ -262,7 +255,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/library/gift.jpg</field>
         </record>
-
         <record id="website.library_image_07" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Website Library Image 07</field>
@@ -271,7 +263,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/library/mobile_device.jpg</field>
         </record>
-
         <record id="website.library_image_08" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Website Library Image 08</field>
@@ -280,7 +271,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/library/mobile.jpg</field>
         </record>
-
         <record id="website.library_image_09" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Website Library Image 09</field>
@@ -289,7 +279,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/library/office.jpg</field>
         </record>
-
         <record id="website.library_image_10" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Website Library Image 10</field>
@@ -298,7 +287,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/library/ice_coffe.jpg</field>
         </record>
-
         <record id="website.library_image_11" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Website Library Image 11</field>
@@ -307,7 +295,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/library/rocket.jpg</field>
         </record>
-
         <record id="website.library_image_12" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Website Library Image 12</field>
@@ -316,7 +303,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/library/sell.jpg</field>
         </record>
-
         <record id="website.library_image_13" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Website Library Image 13</field>
@@ -325,7 +311,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/library/shop.jpg</field>
         </record>
-
         <record id="website.library_image_14" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Website Library Image 14</field>
@@ -334,7 +319,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/library/sign.jpg</field>
         </record>
-
         <record id="website.library_image_15" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Website Library Image 15</field>
@@ -343,7 +327,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/library/sweet.jpg</field>
         </record>
-
         <record id="website.library_image_16" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Website Library Image 16</field>
@@ -427,9 +410,7 @@
             <field name="url">/website/static/src/img/backgrounds/sails.jpg</field>
         </record>
 
-
         <!-- Snippets' Default Images (to be replaced by themes) -->
-
         <record id="website.s_cover_default_image" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Cover Snippet - Default Image</field>
@@ -437,7 +418,6 @@
             <field name="type">url</field>
             <field name="url">/web/image/website.s_background_image_03</field>
         </record>
-
         <record id="website.s_parallax_slider_default_image" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Parallax Slider Snippet - Default Image</field>
@@ -445,7 +425,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/snippets_demo/s_parallax_slider.png</field>
         </record>
-
         <record id="website.s_image_text_default_image" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Image Text Snippet - Default Image</field>
@@ -453,7 +432,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/snippets_demo/s_image_text.png</field>
         </record>
-
         <record id="website.s_text_image_default_image" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Text Image Snippet - Default Image</field>
@@ -461,7 +439,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/snippets_demo/s_text_image.png</field>
         </record>
-
         <record id="website.s_image_floating_default_image" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Image Floating Snippet - Default Image</field>
@@ -469,7 +446,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/snippets_demo/s_image_floating.png</field>
         </record>
-
         <record id="website.s_banner_default_image" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Slider Snippet - Default Image</field>
@@ -477,7 +453,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/snippets_demo/s_banner.png</field>
         </record>
-
         <record id="website.s_big_picture_default_image" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Big Image Snippet - Default Image</field>
@@ -485,7 +460,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/snippets_demo/s_big_picture.png</field>
         </record>
-
         <record id="website.s_reference_demo_image_1" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Reference Snippet - Demo Image 1</field>
@@ -493,7 +467,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/snippets_demo/s_references_1.png</field>
         </record>
-
         <record id="website.s_reference_demo_image_2" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Reference Snippet - Demo Image 2</field>
@@ -501,7 +474,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/snippets_demo/s_references_2.png</field>
         </record>
-
         <record id="website.s_reference_demo_image_3" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Reference Snippet - Demo Image 3</field>
@@ -509,7 +481,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/snippets_demo/s_references_3.png</field>
         </record>
-
         <record id="website.s_reference_demo_image_4" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Reference Snippet - Demo Image 4</field>
@@ -517,7 +488,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/snippets_demo/s_references_4.png</field>
         </record>
-
         <record id="website.s_reference_demo_image_5" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Reference Snippet - Demo Image 5</field>
@@ -525,7 +495,6 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/snippets_demo/s_references_5.png</field>
         </record>
-
         <record id="website.s_company_team_image_1" model="ir.attachment">
             <field name="public" eval="True"/>
             <field name="name">Company Team Snippet - Image 1</field>
@@ -554,6 +523,5 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/snippets_demo/s_team_member_4.png</field>
         </record>
-
     </data>
 </odoo>

commit b5f26c78d37d6e0fc7dd48d5c40f2223941218a6

[FIX] web_editor: properly remove all bg when choosing a bg color
The code which removed the background image of a snippet when choosing a
background color worked kinda by chance. Indeed, on the final click on
the color option, the snippet background-image was reset... to the
current background-image, which is none thanks to the color preview.
Unfortunately, in some themes, the gradient color option interferes:
the current background-image at a color choice is equal to the current
gradient, and thus, that gradient is being moved as an inline style.

The bug was introduced with https://github.com/odoo/odoo/commit/451d814e0ba0f942ebf3bce665409e4a41816268
(as, before, the background reset was done by simulating a click on the
'background: none' option item, which was removed).

Closes https://github.com/odoo/odoo/pull/27808

addons/web_editor/static/src/js/editor/snippets.options.js
@@ -899,7 +899,7 @@ registry.background = SnippetOption.extend({
      * @param {string} value
      */
     _setCustomBackground: function (value) {
-        this.__customImageSrc = this._getSrcFromCssValue(value);
+        this.__customImageSrc = value;
         this.background(false, this.__customImageSrc);
         this.$target.addClass('oe_custom_bg');
         this._setActive();