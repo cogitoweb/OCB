0ef7947ba0d33d4519daf46a5dc8376c0ccbad45
2zx <paolo.cazzitti@cogitoweb.it>
2022-08-24 09:13:08 +0200

    revert

--- odoo/api.py
+++ odoo/api.py
@@ -801,11 +801,9 @@ class Environment(Mapping):
         """
         cls._local.environments = Environments()
 
-    def __new__(cls, cr, uid, context, su=False):
-        if uid == SUPERUSER_ID:
-            su = True
+    def __new__(cls, cr, uid, context):
         assert context is not None
-        args = (cr, uid, context, su)
+        args = (cr, uid, context)
 
         # if env already exists, return it
         env, envs = None, cls.envs
@@ -815,11 +813,12 @@ class Environment(Mapping):
 
         # otherwise create environment, and add it in the set
         self = object.__new__(cls)
-        args = (cr, uid, frozendict(context), su)
-        self.cr, self.uid, self.context, self.su = self.args = args
+        self.cr, self.uid, self.context = self.args = (cr, uid, frozendict(context))
         self.registry = Registry(cr.dbname)
         self.cache = envs.cache
-        self._cache_key = {}                    # memo {field: cache_key}
+        self._cache_key = (cr, uid)
+        self._protected = StackMap()                # {field: ids, ...}
+        self.dirty = defaultdict(set)               # {record: set(field_name), ...}
         self.all = envs
         envs.add(self)
         return self
@@ -853,7 +852,7 @@ class Environment(Mapping):
     def __hash__(self):
         return object.__hash__(self)
 
-    def __call__(self, cr=None, user=None, context=None, su=None):
+    def __call__(self, cr=None, user=None, context=None):
         """ Return an environment based on ``self`` with modified parameters.
 
             :param cr: optional database cursor to change the current cursor
@@ -863,8 +862,7 @@ class Environment(Mapping):
         cr = self.cr if cr is None else cr
         uid = self.uid if user is None else int(user)
         context = self.context if context is None else context
-        su = (user is None and self.su) if su is None else su
-        return Environment(cr, uid, context, su)
+        return Environment(cr, uid, context)
 
     def ref(self, xml_id, raise_if_not_found=True):
         """ return the record corresponding to the given ``xml_id`` """
