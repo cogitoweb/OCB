0a55e2bcbf8d601e46a7aa7adbe4f8f9db402658
2zx <paolo.cazzitti@cogitoweb.it>
2022-08-24 14:56:59 +0200

    test

--- odoo/models.py
+++ odoo/models.py
@@ -25,6 +25,7 @@ import datetime
 
 import collections
 import dateutil
+import fnmatch
 import functools
 import itertools
 import io
@@ -4957,6 +4958,106 @@ class BaseModel(MetaModel('DummyModel', (object,), {'_register': False})):
             name = func
             func = lambda rec: any(rec.mapped(name))
         return self.browse([rec.id for rec in self if func(rec)])
+
+    def filtered_domain(self, domain):
+        if not domain: return self
+        result = []
+        for d in reversed(domain):
+            if d == '|':
+                result.append(result.pop() | result.pop())
+            elif d == '!':
+                result.append(self - result.pop())
+            elif d == '&':
+                result.append(result.pop() & result.pop())
+            elif d == expression.TRUE_LEAF:
+                result.append(self)
+            elif d == expression.FALSE_LEAF:
+                result.append(self.browse())
+            else:
+                (key, comparator, value) = d
+                if comparator in ('child_of', 'parent_of'):
+                    result.append(self.search([('id', 'in', self.ids), d]))
+                    continue
+                if key.endswith('.id'):
+                    key = key[:-3]
+                if key == 'id':
+                    key = ''
+                # determine the field with the final type for values
+                field = None
+                if key:
+                    model = self.browse()
+                    for fname in key.split('.'):
+                        field = model._fields[fname]
+                        model = model[fname]
+                if comparator in ('like', 'ilike', '=like', '=ilike', 'not ilike', 'not like'):
+                    value_esc = value.replace('_', '?').replace('%', '*').replace('[', '?')
+                records_ids = OrderedSet()
+                for rec in self:
+                    data = rec.mapped(key)
+                    if isinstance(data, BaseModel):
+                        v = value
+                        if (isinstance(value, list) or isinstance(value, tuple)) and len(value):
+                            v = value[0]
+                        if isinstance(v, str):
+                            data = data.mapped('display_name')
+                        else:
+                            data = data and data.ids or [False]
+                    elif field and field.type in ('date', 'datetime'):
+                        # convert all date and datetime values to datetime
+                        normalize = Datetime.to_datetime
+                        if isinstance(value, (list, tuple)):
+                            value = [normalize(v) for v in value]
+                        else:
+                            value = normalize(value)
+                        data = [normalize(d) for d in data]
+                    if comparator in ('in', 'not in'):
+                        if not (isinstance(value, list) or isinstance(value, tuple)):
+                            value = [value]
+
+                    if comparator == '=':
+                        ok = value in data
+                    elif comparator == 'in':
+                        ok = any(map(lambda x: x in data, value))
+                    elif comparator == '<':
+                        ok = any(map(lambda x: x is not None and x < value, data))
+                    elif comparator == '>':
+                        ok = any(map(lambda x: x is not None and x > value, data))
+                    elif comparator == '<=':
+                        ok = any(map(lambda x: x is not None and x <= value, data))
+                    elif comparator == '>=':
+                        ok = any(map(lambda x: x is not None and x >= value, data))
+                    elif comparator in ('!=', '<>'):
+                        ok = value not in data
+                    elif comparator == 'not in':
+                        ok = all(map(lambda x: x not in data, value))
+                    elif comparator == 'not ilike':
+                        data = [(x or "") for x in data]
+                        ok = all(map(lambda x: value.lower() not in x.lower(), data))
+                    elif comparator == 'ilike':
+                        data = [(x or "").lower() for x in data]
+                        ok = bool(fnmatch.filter(data, '*'+(value_esc or '').lower()+'*'))
+                    elif comparator == 'not like':
+                        data = [(x or "") for x in data]
+                        ok = all(map(lambda x: value not in x, data))
+                    elif comparator == 'like':
+                        data = [(x or "") for x in data]
+                        ok = bool(fnmatch.filter(data, value and '*'+value_esc+'*'))
+                    elif comparator == '=?':
+                        ok = (value in data) or not value
+                    elif comparator in ('=like'):
+                        data = [(x or "") for x in data]
+                        ok = bool(fnmatch.filter(data, value_esc))
+                    elif comparator in ('=ilike'):
+                        data = [(x or "").lower() for x in data]
+                        ok = bool(fnmatch.filter(data, value and value_esc.lower()))
+                    else:
+                        raise ValueError
+                    if ok:
+                       records_ids.add(rec.id)
+                result.append(self.browse(records_ids))
+        while len(result)>1:
+            result.append(result.pop() & result.pop())
+        return result[0]
 
     def sorted(self, key=None, reverse=False):
         """ Return the recordset ``self`` ordered by ``key``.
