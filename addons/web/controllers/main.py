685fdb23d6d1763cc47785cc0c12106d756e601d
2zx <paolo.cazzitti@cogitoweb.it>
2023-09-18 14:58:22 +0200

    various

--- addons/web/controllers/main.py
+++ addons/web/controllers/main.py
@@ -78,7 +78,7 @@ def serialize_exception(f):
             se = _serialize_exception(e)
             error = {
                 'code': 200,
-                'message': "Odoo Server Error",
+                'message': "Server Error",
                 'data': se
             }
             return werkzeug.exceptions.InternalServerError(json.dumps(error))
