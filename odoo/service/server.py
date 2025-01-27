55caa45af1c078f999de1bdcd9d4a24427f55b70
Davide Viotto <davide.viotto@cogitoweb.it>
2025-01-27 17:32:15 +0100

    Cache fix

--- odoo/service/server.py
+++ odoo/service/server.py
@@ -275,7 +275,7 @@ class ThreadedServer(CommonServer):
             time.sleep(SLEEP_INTERVAL + number)     # Steve Reich timing style
             registries = odoo.modules.registry.Registry.registries
             _logger.debug('cron%d polling for jobs', number)
-            for db_name, registry in registries.items():
+            for db_name, registry in registries.d.items():
                 if registry.ready:
                     try:
                         odoo.addons.base.ir.ir_cron.ir_cron._acquire_job(db_name)
