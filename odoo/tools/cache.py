239af33e3d5d6ee00d68f901408f60826420d9e3
Davide Viotto <davide.viotto@cogitoweb.it>
2025-02-04 09:26:14 +0100

    Fix cache

--- odoo/tools/cache.py
+++ odoo/tools/cache.py
@@ -96,7 +96,9 @@ class ormcache(object):
 
     def clear(self, model, *args):
         """ Clear the registry cache """
-        model.pool._clear_cache()
+        d, key0, _ = self.lru(model)
+        d.clear()
+        model.pool.cache_cleared = True
 
 
 class ormcache_context(ormcache):
