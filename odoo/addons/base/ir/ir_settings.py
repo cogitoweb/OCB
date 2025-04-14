51ebfac11d1ac7b0fd6ee717cb87f9ba8e0d6170
Davide Viotto <davide.viotto@cogitoweb.it>
2025-04-14 15:24:38 +0200

    Add more function from ir_values

--- odoo/addons/base/ir/ir_settings.py
+++ odoo/addons/base/ir/ir_settings.py
@@ -1,3 +1,4 @@
+from typing import Self
 from odoo import api, fields, models, tools, _
 from odoo.exceptions import AccessError, MissingError
 
@@ -19,6 +20,25 @@ class IrSettings(models.Model):
     values = fields.Json(name="Values")
 
     @api.model
+    def create(self, vals):
+        self.clear_caches()
+        return super(IrSettings, self).create(vals)
+
+    @api.multi
+    def write(self, vals):
+        self.clear_caches()
+        return super(IrSettings, self).write(vals)
+
+    @api.multi
+    def unlink(self):
+        self.clear_caches()
+        return super(IrSettings, self).unlink()
+
+    @api.model
+    def get_defaults(self, model: str, condition=False) -> list:
+        return list(self.get_model_settings(model).items())
+
+    @api.model
     def get_default(self, model: str, field: str):
         values = self.get_model_settings(model)
         return values.get(field, None)
@@ -31,11 +51,16 @@ class IrSettings(models.Model):
         return record.values
 
     @api.model
-    def set_default(self, model: str, field: str, value) -> None:
+    def set_default(self, model: str, field: str, value) -> Self:
         record = self.search([('model', '=', model)], limit=1)
         if not record:
             record = self.create({'model': model, 'values': {field: value}})
         current_values = record.values
         current_values[field] = value
         record.values = current_values
-        return
+        return record
+
+    @api.model
+    @tools.ormcache('self._uid', 'model')
+    def get_defaults_dict(self, model: str, condition=False) -> dict:
+        return dict((f, v) for i, f, v in self.get_defaults(model, condition))
