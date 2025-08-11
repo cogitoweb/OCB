633169f4cf29c1694d115072d8f40c866edef3dc
Paolo Cazzitti <paolo.cazzitti@cogitoweb.it>
2025-08-11 15:22:13 +0200

    test

--- odoo/tools/safe_eval.py
+++ odoo/tools/safe_eval.py
@@ -423,7 +423,7 @@ def check_values(d):
     if not d:
         return d
     for v in d.values():
-        if isinstance(v, types.ModuleType):
+        if isinstance(v, types.ModuleType) and "odoo" not in str(v):
             raise TypeError(f"""Module {v} can not be used in evaluation contexts
 
 Prefer providing only the items necessary for your intended use.
