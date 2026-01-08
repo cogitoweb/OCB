e8268baaccbde16ff26fa93fb81047960a9dc96a
Onyx Ganda <onyx.ganda@cartesia.dev>
2026-01-08 11:35:01 +0100

    fix comparison when probability is missing

--- addons/crm/models/crm_lead.py
+++ addons/crm/models/crm_lead.py
@@ -320,7 +320,7 @@ class Lead(FormatAddress, models.Model):
         # stage change with new stage: update probability and date_closed
         if vals.get('stage_id') and 'probability' not in vals:
             vals.update(self._onchange_stage_id_values(vals.get('stage_id')))
-        if vals.get('probability') >= 100 or not vals.get('active', True):
+        if vals.get('probability', 0) >= 100 or not vals.get('active', True):
             vals['date_closed'] = fields.Datetime.now()
         elif 'probability' in vals and vals['probability'] < 100:
             vals['date_closed'] = False
